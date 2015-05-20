0881f08f6c0068302089d22cfd75dfae6b1c71aa
Thiago da Cruz <tdacruzper@ripe.net>
2015-05-20 15:10:41 +0200

    Inject WhoisMetaService into the spec class

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -1,26 +1,12 @@
 'use strict';
 
-angular.module('dbWebuiApp', ['ngRoute'])
-.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/whoisobject/select', {
-        templateUrl: 'scripts/app/select.html',
-        controller: 'SelectController'
-      }).
-      when('/whoisobject/create/:objectType/:source', {
-        templateUrl: 'scripts/app/create.html',
-        controller: 'CreateController'
-      }).
-      when('/whoisobject/modify/:objectType/:objectUid', {
-        templateUrl: 'modify.html',
-        controller: 'ModifyController'
-      }).
-      when('/whoisobject/display/:objectType/:objectUid', {
-        templateUrl: 'scripts/app/display.html',
-        controller: 'DisplayController'
-      }).
-      otherwise({
-        redirectTo: '/whoisobject/select'
-      });
-  }]);
+angular.module('dbWebuiApp', ['ui.router'])
+.config(function ($stateProvider) {
+
+    $stateProvider
+        .state('select', {
+            url: '/select',
+            templateUrl: 'scripts/app/select.html',
+            controller: 'CreateController'
+        });
+});
--- src/test/javascript/spec/whoisMetaServiceSpec.js
+++ src/test/javascript/spec/whoisMetaServiceSpec.js
@@ -1,26 +1,23 @@
 'use strict';
 
-describe('dbWebuiApp: whoisMetaService', function () {
-    var $rootScope, $scope, $window, $controller, $httpBackend;
+describe('dbWebuiApp: WhoisMetaService', function () {
+
+    var $whoisMetaService;
 
     beforeEach(module('dbWebuiApp'));
 
-    //beforeEach(inject(function (_$rootScope_, _$controller_, _$window_, _$httpBackend_) {
-    //    $rootScope = _$rootScope_;
-    //    $scope = $rootScope.$new();
-    //    $window = _$window_;
-    //    $controller = _$controller_;
-    //    $httpBackend = _$httpBackend_;
-    //
-    //    //$controller('FindMaintainerCtrl', {$scope: $scope, $window: $window, Maintainer: Maintainer, Validate: Validate, SendMail: SendMail});
-    //}));
+    beforeEach(inject(function (WhoisMetaService) {
+        $whoisMetaService = WhoisMetaService;
+
+    }));
 
     afterEach(function() {
 
     });
 
     it('should make true equals true', function() {
-        expect(true).toBe(true)
+
+        console.log($whoisMetaService.getObjectTypes())
     });
 
     it('should return all objectTypes', function(){
