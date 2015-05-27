5b2ee1cdc190075f43c03f583e3950be7bb41aaa
Theodoros Polychniatis <tpolychnia@ripe.net>
2015-05-27 11:14:11 +0200

    first version of showing messages

--- src/main/webapp/scripts/app/createController.js
+++ src/main/webapp/scripts/app/createController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('dbWebuiApp')
-.controller('CreateController', ['$scope', '$stateParams', '$state', 'WhoisMetaService', 'WhoisRestService',
-function ($scope, $stateParams, $state, WhoisMetaService, WhoisRestService) {
+.controller('CreateController', ['$scope', '$stateParams', '$state', '$resource', 'WhoisMetaService', 'WhoisRestService',
+function ($scope, $stateParams, $state, $resource, WhoisMetaService, WhoisRestService) {
 
     // extract parameters from the url
     $scope.objectType = $stateParams.objectType;
@@ -11,6 +11,8 @@ function ($scope, $stateParams, $state, WhoisMetaService, WhoisRestService) {
     // Initalize the UI
     $scope.errors = [];
     $scope.warnings = [];
+    $scope.infos = [];
+
     var allAttributesOnObjectType = WhoisMetaService.getMandatoryAttributesOnObjectType($scope.objectType);
     $scope.attributes = _.map(allAttributesOnObjectType, function (x) {
         return {name: x.name, value: x.value, $$mandatory: x.mandatory}
@@ -36,6 +38,10 @@ function ($scope, $stateParams, $state, WhoisMetaService, WhoisRestService) {
 
     $scope.hasWarnings = function () {
         return $scope.warnings.length > 0;
+    };
+
+    $scope.hasInfos = function () {
+        return $scope.infos.length > 0;
     };
 
     $scope.attributeHasError = function (attribute) {
@@ -66,16 +72,47 @@ function ($scope, $stateParams, $state, WhoisMetaService, WhoisRestService) {
         return errorFound == false;
     };
 
+    //$scope.submit = function () {
+    //    if ($scope.validateForm() == true) {
+    //        $scope.response = WhoisRestService.createObject($scope.source, $scope.objectType, $scope.objects);
+    //
+    //        console.log('$scope.response======>' + response);
+    //
+    //        $scope.errors = WhoisRestService.getErrors();
+    //        $scope.warnings = WhoisRestService.getWarnings();
+    //        if ($scope.hasErrors() == false) {
+    //            $scope.clearAttributeErrors();
+    //            changeToStep('display');
+    //        }
+    //
+    //    }
+    //};
+
+    // remove me
+
     $scope.submit = function () {
         if ($scope.validateForm() == true) {
-            $scope.response = WhoisRestService.createObject($scope.source, $scope.objectType, $scope.objects);
-            $scope.errors = WhoisRestService.getErrors();
-            $scope.warnings = WhoisRestService.getWarnings();
+            //$scope.response = WhoisRestService.createObject($scope.source, $scope.objectType, $scope.objects);
+            $resource('whois/:source/:objectType', {source: $scope.source, objectType: $scope.objectType}).save($scope.objects,
+                function(response){
+                    console.log('SSSS.stringify data=======>'+JSON.stringify(response));
+                    console.log('response status:' + response)
+                    $scope.attributes = response.objects.object[0].attributes.attribute;
+                    $scope.errors = [];
+                    $scope.warnings = [];
+                    $scope.infos[0] = 'Success! Go to the next level :)';
+                },
+                function(response){
+                    console.log('EEEE.JSON.stringify(response)=======>'+JSON.stringify(response.data));
+                    $scope.errors = response.data.errormessages.errormessage;
+                    $scope.warnings = [];
+                    console.log('error response status:' + response.status)
+                });
             if ($scope.hasErrors() == false) {
+                console.log('going to display')
                 $scope.clearAttributeErrors();
                 changeToStep('display');
             }
-
         }
     };
 
