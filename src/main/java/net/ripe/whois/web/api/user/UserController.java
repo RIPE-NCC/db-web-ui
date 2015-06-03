8f838dac8d917953cf0e2ee59dba135201d272ca
Edward Shryane <eshryane@ripe.net>
2015-06-03 11:21:05 +0200

    Return ResponseEntity<String> from backend (don't deserialise and serialise, which is slow, and will also change the response (in particular, empty JSON arrays))

--- src/main/java/net/ripe/whois/web/api/user/UserController.java
+++ src/main/java/net/ripe/whois/web/api/user/UserController.java
@@ -1,16 +1,17 @@
 package net.ripe.whois.web.api.user;
 
-import net.ripe.db.whois.api.rest.domain.WhoisResources;
 import net.ripe.db.whois.common.sso.CrowdClient;
 import net.ripe.db.whois.common.sso.CrowdClientException;
 import net.ripe.db.whois.common.sso.UserSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
-import static org.springframework.http.ResponseEntity.status;
 
 
 @RestController
@@ -24,14 +25,14 @@ public class UserController {
     private WhoisInternalClient whoisInternalClient;
 
     @RequestMapping(value = "/maintainers", method = RequestMethod.GET)
-    public ResponseEntity<WhoisResources> getMaintainers(@CookieValue(value = "crowd.token_key", required = true) final String crowdToken)throws Exception {
+    public ResponseEntity getMaintainers(@CookieValue(value = "crowd.token_key", required = true) final String crowdToken)throws Exception {
         final UserSession userSession;
 
         try {
             userSession = crowdClient.getUserSession(crowdToken);
             userSession.setUuid(crowdClient.getUuid(userSession.getUsername()));
         } catch (CrowdClientException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
 
         return ok(whoisInternalClient.getMaintainers(userSession.getUuid()));
--- src/main/java/net/ripe/whois/web/api/user/WhoisInternalClient.java
+++ src/main/java/net/ripe/whois/web/api/user/WhoisInternalClient.java
@@ -1,20 +1,50 @@
 package net.ripe.whois.web.api.user;
 
-import com.google.common.collect.Lists;
-import net.ripe.db.whois.api.rest.domain.Attribute;
-import net.ripe.db.whois.api.rest.domain.WhoisObject;
-import net.ripe.db.whois.api.rest.domain.WhoisResources;
+import com.google.common.collect.Maps;
+import net.ripe.whois.web.api.whois.RestResponseErrorHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @Component
 public class WhoisInternalClient {
-    public WhoisResources getMaintainers(String uuid) {
-        WhoisObject whoisObject = new WhoisObject();
 
-        whoisObject.setAttributes(Lists.newArrayList(new Attribute("mntner", "GROLSSO-MNT"),
-            new Attribute("auth", "SSO", "Filtered", null, null),
-            new Attribute("source", "RIPE", "Filtered", null, null)));
+    private final String apiUrl;
+    private final String apiKey;
+    private final RestTemplate restTemplate;
 
-        return new WhoisResources().setWhoisObjects(Lists.newArrayList(whoisObject));
+    @Autowired
+    public WhoisInternalClient(@Value("${internal.api.url}") final String apiUrl,  @Value("${internal.api.key}") final String apiKey) {
+        this.apiUrl = apiUrl;
+        this.apiKey = apiKey;
+        this.restTemplate = createRestTemplate();
     }
+
+    public ResponseEntity<String> getMaintainers(String uuid) {
+        final HashMap<String, String> variables = Maps.newHashMap();
+        variables.put("apiUrl", apiUrl);
+        variables.put("apiKey", apiKey);
+        variables.put("uuid", uuid);
+        return restTemplate.getForEntity(
+            "{apiUrl}/api/user/{uuid}/maintainers?apiKey={apiKey}",
+            String.class,
+            variables);
+    }
+
+    private RestTemplate createRestTemplate() {
+        final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        final ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        final RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.setErrorHandler(new RestResponseErrorHandler());
+        return restTemplate;
+    }
+
 }
