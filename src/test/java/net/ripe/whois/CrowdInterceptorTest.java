25be42d1b0e827e06e5b35e7633157ab66966177
Thiago da Cruz <tdacruzper@ripe.net>
2015-06-08 17:08:09 +0200

    Remove crowd cache.

--- src/test/java/net/ripe/whois/CrowdInterceptorTest.java
+++ src/test/java/net/ripe/whois/CrowdInterceptorTest.java
@@ -1,14 +1,7 @@
 package net.ripe.whois;
 
-
-import net.ripe.db.whois.common.sso.CrowdClient;
-import net.ripe.db.whois.common.sso.CrowdClientException;
-import net.ripe.db.whois.common.sso.UserSession;
-import net.ripe.whois.web.api.user.UserController;
-import org.joda.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
@@ -16,112 +9,43 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.Cookie;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.*;
 
 public class CrowdInterceptorTest {
     public static final String CROWD_TOKEN = "SOME_CROWD_TOKEN";
 
-
-    /*
-     HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-        try {
-            UserSession user = crowdClient.getUserSession(getCookie(request));
-            UserController.setUserSession(request, user);
-            chain.doFilter(req, res);
-        } catch (CrowdClientException e) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-        }
-     */
-
-    private CrowdClient crowdClient;
     private FilterChain filterChain;
 
     private MockHttpServletResponse response;
     private MockHttpServletRequest request;
 
     private CrowdInterceptor crowdInterceptor;
-    private UserSession userSession = new UserSession("someone@ripe.net", true, null);
 
     @Before
     public void setup() {
-
-        crowdClient = mock(CrowdClient.class);
         filterChain = mock(FilterChain.class);
 
         response = new MockHttpServletResponse();
         request = new MockHttpServletRequest();
 
-        crowdInterceptor = new CrowdInterceptor(crowdClient);
-
-        request.setCookies(new Cookie(CrowdInterceptor.CROWD_TOKEN_KEY, CROWD_TOKEN));
-    }
-
-    @Test
-    public void itShouldStoreTheUserSessionInTheRequest() throws Exception {
-        when(crowdClient.getUserSession(CROWD_TOKEN)).thenReturn(userSession);
-
-        crowdInterceptor.doFilter(request, response, filterChain);
-
-        assertSame(userSession, request.getAttribute(UserController.REQUEST_USER));
-
+        crowdInterceptor = new CrowdInterceptor();
     }
 
     @Test
     public void itShouldProceedWithDoFilter() throws Exception {
+        request.setCookies(new Cookie(CrowdInterceptor.CROWD_TOKEN_KEY, CROWD_TOKEN));
+
         crowdInterceptor.doFilter(request, response, filterChain);
 
         verify(filterChain).doFilter(request, response);
     }
 
     @Test
-    public void itShouldGive401OnCrowExceptions() throws Exception {
-        when(crowdClient.getUserSession(CROWD_TOKEN)).thenThrow(new CrowdClientException(""));
+    public void itShouldGive401IfCookieIsNotPresent() throws Exception {
+        request.setCookies();
 
         crowdInterceptor.doFilter(request, response, filterChain);
 
         assertEquals(401, response.getStatus());
     }
-
-    @Test
-    public void itShouldNotProceedWithTheChainOnCrowExceptions() throws Exception {
-        when(crowdClient.getUserSession(CROWD_TOKEN)).thenThrow(new CrowdClientException(""));
-
-        crowdInterceptor.doFilter(request, response, filterChain);
-
-        verifyZeroInteractions(filterChain);
-    }
-
-    @Test
-    public void itShouldUseEmptyStringIfCookieIsNotPresent() throws Exception {
-        request.setCookies();
-
-        crowdInterceptor.doFilter(request, response, filterChain);
-
-        verify(crowdClient).getUserSession("");
-    }
-
-    @Test
-    public void itShouldRetrieveUserSessionOnceWhileValid() throws Exception {
-        when(crowdClient.getUserSession(CROWD_TOKEN)).thenReturn(userSession);
-
-        crowdInterceptor.doFilter(request, response, filterChain);
-        crowdInterceptor.doFilter(request, response, filterChain);
-
-        verify(crowdClient, times(1)).getUserSession(CROWD_TOKEN);
-    }
-
-    @Test
-    public void itShouldRetrieveUserSessionAgainIfExpired() throws Exception {
-        LocalDateTime expiryDate = LocalDateTime.now().minusHours(1);
-        when(crowdClient.getUserSession(CROWD_TOKEN)).thenReturn(new UserSession("someone@ripe.net", true, expiryDate.toString()));
-
-        crowdInterceptor.doFilter(request, response, filterChain);
-        crowdInterceptor.doFilter(request, response, filterChain);
-
-        verify(crowdClient, times(2)).getUserSession(CROWD_TOKEN);
-    }
-
 }
