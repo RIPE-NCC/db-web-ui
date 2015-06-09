184d78a9c4335a3acbc6451e4ed949d4b96edc7a
Marc Grol <mgrol@xebia.com>
2015-06-09 12:35:55 +0200

    Removed logs

--- src/main/webapp/scripts/app/webupdates/displayController.js
+++ src/main/webapp/scripts/app/webupdates/displayController.js
@@ -16,12 +16,9 @@ function ($scope, $stateParams, $state, $resource, WhoisResources, MessageStore)
     var fetchObject = function() {
         $resource('api/whois/:source/:objectType/:objectName', {source: $scope.objectSource, objectType: $scope.objectType, objectName:$scope.objectName})
             .get(function (resp) {
-                //console.log("web ok resp:" + JSON.stringify(resp));
-
                 var whoisResources = WhoisResources.wrapWhoisResources(resp);
                 $scope.attributes = WhoisResources.wrapAttributes(whoisResources.getAttributes());
             }, function(resp) {
-                //console.log("web error resp:" + JSON.stringify(resp));
                 var whoisResources = WhoisResources.wrapWhoisResources(resp.data);
                 if( ! _.isUndefined(whoisResources)) {
                     $scope.errors = whoisResources.getGlobalErrors();
@@ -34,12 +31,10 @@ function ($scope, $stateParams, $state, $resource, WhoisResources, MessageStore)
     var cashed = MessageStore.get($scope.objectName);
     if( cashed) {
         var whoisResources = WhoisResources.wrapWhoisResources(cashed);
-        //console.log("Using the store:" + JSON.stringify(whoisResources));
         // Use version that we was just before created or modified
         $scope.attributes = WhoisResources.wrapAttributes(whoisResources.getAttributes());
         $scope.warnings = whoisResources.getGlobalWarnings();
     } else {
-        //console.log("Fetch from web");
         fetchObject();
     }
 
