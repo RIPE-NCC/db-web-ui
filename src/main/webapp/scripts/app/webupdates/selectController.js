027f7878f50405b057f2141fedd8b04ea86abd53
Marc Grol <mgrol@xebia.com>
2015-06-09 19:32:49 +0200

    use  state like everywhere else

--- src/main/webapp/scripts/app/webupdates/selectController.js
+++ src/main/webapp/scripts/app/webupdates/selectController.js
@@ -1,15 +1,14 @@
 'use strict';
 
 angular.module('webUpdates')
-.controller('SelectController', ['$scope', '$location', 'WhoisMetaService',
-	function ($scope, $location, WhoisMetaService) {
+.controller('SelectController', ['$scope', '$state', 'WhoisMetaService',
+	function ($scope, $state, WhoisMetaService) {
         $scope.objectTypes   = WhoisMetaService.getObjectTypes();
         $scope.selectedObjectType = $scope.objectTypes[0];
         $scope.sources   = ['RIPE','TEST'];
         $scope.selectedSource = $scope.sources[0];
 
         $scope.navigateToCreate = function() {
-          $location.path( '/webupdates/create/' + $scope.selectedObjectType + '/' + $scope.selectedSource );
-          //NavigationService.navigateToCreate( $scope.selectedObjectType, $scope.selectedSource );
+            $state.transitionTo('create', {source:$scope.selectedSource, objectType:$scope.selectedObjectType});
         };
 }]);
