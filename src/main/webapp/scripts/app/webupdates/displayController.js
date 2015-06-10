4d9ebc7eb0f14c8bcd1b5fa1dc2a48afbbda0e4f
Marc Grol <mgrol@xebia.com>
2015-06-10 16:15:18 +0200

    reformat

--- src/main/webapp/scripts/app/webupdates/displayController.js
+++ src/main/webapp/scripts/app/webupdates/displayController.js
@@ -1,64 +1,61 @@
 'use strict';
 
 angular.module('webUpdates')
-.controller('DisplayController', ['$scope', '$stateParams', '$state', '$resource', 'WhoisResources','MessageStore',
-function ($scope, $stateParams, $state, $resource, WhoisResources, MessageStore) {
+    .controller('DisplayController', ['$scope', '$stateParams', '$state', '$resource', 'WhoisResources', 'MessageStore',
+        function ($scope, $stateParams, $state, $resource, WhoisResources, MessageStore) {
 
-    // extract parameters from the url
-    $scope.objectSource = $stateParams.source;
-    $scope.objectType = $stateParams.objectType;
-    $scope.objectName = $stateParams.name;
+            // extract parameters from the url
+            $scope.objectSource = $stateParams.source;
+            $scope.objectType = $stateParams.objectType;
+            $scope.objectName = $stateParams.name;
 
-    // Initalize the UI
-    $scope.errors = [];
-    $scope.warnings = [];
-    $scope.infos = [];
+            // Initalize the UI
+            $scope.errors = [];
+            $scope.warnings = [];
+            $scope.infos = [];
 
-    var fetchObjectViaRest = function() {
-        $resource('api/whois/:source/:objectType/:objectName', {source: $scope.objectSource, objectType: $scope.objectType, objectName:$scope.objectName})
-            .get(function (resp) {
-                var whoisResources = WhoisResources.wrapWhoisResources(resp);
+            var fetchObjectViaRest = function () {
+                $resource('api/whois/:source/:objectType/:objectName', {
+                    source: $scope.objectSource,
+                    objectType: $scope.objectType,
+                    objectName: $scope.objectName
+                })
+                    .get(function (resp) {
+                        var whoisResources = WhoisResources.wrapWhoisResources(resp);
+                        $scope.attributes = WhoisResources.wrapAttributes(whoisResources.getAttributes());
+                    }, function (resp) {
+                        var whoisResources = WhoisResources.wrapWhoisResources(resp.data);
+                        if (!_.isUndefined(whoisResources)) {
+                            $scope.errors = whoisResources.getGlobalErrors();
+                            $scope.warnings = whoisResources.getGlobalWarnings();
+                            $scope.infos = whoisResources.getGlobalInfos();
+
+                        }
+                    });
+            };
+
+            // fetch just created object from temporary store
+            var cached = MessageStore.get($scope.objectName);
+            if (cached) {
+                var whoisResources = WhoisResources.wrapWhoisResources(cached);
+                // Use version that we was just before created or modified
                 $scope.attributes = WhoisResources.wrapAttributes(whoisResources.getAttributes());
-            }, function(resp) {
-                var whoisResources = WhoisResources.wrapWhoisResources(resp.data);
-                if( ! _.isUndefined(whoisResources)) {
-                    $scope.errors = whoisResources.getGlobalErrors();
-                    $scope.warnings = whoisResources.getGlobalWarnings();
-                    $scope.infos = whoisResources.getGlobalInfos();
+                $scope.warnings = whoisResources.getGlobalWarnings();
+                $scope.infos = whoisResources.getGlobalInfos();
+            } else {
+                fetchObjectViaRest();
+            }
 
-                }
-            });
-    };
+            $scope.navigateToSelect = function () {
+                $state.transitionTo('select');
+            };
 
-    // fetch just created object from temporary store
-    var cached = MessageStore.get($scope.objectName);
-    if( cached) {
-        var whoisResources = WhoisResources.wrapWhoisResources(cached);
-        // Use version that we was just before created or modified
-        $scope.attributes = WhoisResources.wrapAttributes(whoisResources.getAttributes());
-        $scope.warnings = whoisResources.getGlobalWarnings();
-        $scope.infos = whoisResources.getGlobalInfos();
-    } else {
-        fetchObjectViaRest();
-    }
+            $scope.navigateToModify = function () {
+                $state.transitionTo('modify', {
+                    source: $scope.objectSource,
+                    objectType: $scope.objectType,
+                    name: $scope.objectName
+                });
+            };
 
-    $scope.navigateToSelect = function () {
-        $state.transitionTo('select');
-    };
-
-    $scope.navigateToModify = function () {
-        /*
-         $state.transitionTo('display', {
-         source: $scope.source,
-         objectType: $scope.objectType,
-         name: whoisResources.getObjectUid()
-         });
-         */
-        $state.transitionTo('modify',  {
-            source:$scope.objectSource,
-            objectType:$scope.objectType,
-            name: $scope.objectName
-        });
-    };
-
-}]);
+        }]);
--- src/main/webapp/scripts/app/webupdates/selectController.js
+++ src/main/webapp/scripts/app/webupdates/selectController.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('webUpdates')
-.controller('SelectController', ['$scope', '$state', 'WhoisMetaService',
-	function ($scope, $state, WhoisMetaService) {
-        $scope.objectTypes   = WhoisMetaService.getObjectTypes();
-        $scope.selectedObjectType = $scope.objectTypes[0];
-        $scope.sources   = ['RIPE','TEST'];
-        $scope.selectedSource = $scope.sources[0];
+    .controller('SelectController', ['$scope', '$state', 'WhoisMetaService',
+        function ($scope, $state, WhoisMetaService) {
+            $scope.objectTypes = WhoisMetaService.getObjectTypes();
+            $scope.selectedObjectType = $scope.objectTypes[0];
+            $scope.sources = ['RIPE', 'TEST'];
+            $scope.selectedSource = $scope.sources[0];
 
-        $scope.navigateToCreate = function() {
-            $state.transitionTo('create', {source:$scope.selectedSource, objectType:$scope.selectedObjectType});
-        };
-}]);
+            $scope.navigateToCreate = function () {
+                $state.transitionTo('create', {source: $scope.selectedSource, objectType: $scope.selectedObjectType});
+            };
+        }]);
