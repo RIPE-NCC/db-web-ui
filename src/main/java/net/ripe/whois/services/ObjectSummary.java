a09f7ca5215ce8595500eaaa8428d8c7b612dfb1
Marc Grol <mgrol@xebia.com>
2015-06-17 17:21:48 +0200

    Finalized compact format for get-maintainmer for cookie

--- src/main/java/net/ripe/whois/services/ObjectSummary.java
+++ src/main/java/net/ripe/whois/services/ObjectSummary.java
@@ -3,15 +3,17 @@ package net.ripe.whois.services;
 import java.util.Map;
 
 class ObjectSummary {
-    private String value;
+    private String primary_key;
+    private String object_type;
     private Map<String, Object> extras;
 
     protected ObjectSummary() {
         // make gson happy
     }
 
-    public ObjectSummary(final String name, Map<String, Object> extras) {
-        this.value = name;
+    public ObjectSummary(final String primary_key, final String object_type, final Map<String, Object> extras) {
+        this.primary_key = primary_key;
+        this.object_type = object_type;
         this.extras = extras;
     }
 
