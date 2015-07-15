2342da05da14701da0449c7a9004fa2e64bf7686
Edward Shryane <eshryane@ripe.net>
2015-07-15 15:05:06 +0200

    Constructor autowiring doesn't work in LoggingAspectConfiguration, use field autowiring instead.

--- src/main/java/net/ripe/whois/config/LoggingAspectConfiguration.java
+++ src/main/java/net/ripe/whois/config/LoggingAspectConfiguration.java
@@ -6,17 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
+// TODO: [ES] constructor autowiring doesn't work here
 @Configuration
 @EnableAspectJAutoProxy
+@Component
 public class LoggingAspectConfiguration {
 
-    private final LoggingAspect loggingAspect;
-
     @Autowired
-    public LoggingAspectConfiguration(final LoggingAspect loggingAspect) {
-        this.loggingAspect = loggingAspect;
-    }
+    private LoggingAspect loggingAspect;
 
     @Bean
     @Profile({Constants.SPRING_PROFILE_DEVELOPMENT, Constants.SPRING_PROFILE_PREPDEV})
