130f887ec3665177db04e0480864986b6f684398
Marc Grol <mgrol@xebia.com>
2015-09-10 18:08:10 +0200

    reshuffle field sent to google tag manager

--- src/main/webapp/scripts/app/errorReporterService.js
+++ src/main/webapp/scripts/app/errorReporterService.js
@@ -1,16 +1,15 @@
 angular.module('dbWebApp')
     .service('ErrorReporterService', ['$log','$location','$analytics', function ($log, $location, $analytics) {
         this.log = function (operation, objectType, globalErrors, attributes) {
-            //$analytics.pageTrack('/my/url');
-            //$analytics.eventTrack('virtualPageView', { category: 'categoryName' });
             _.each(globalErrors, function(item) {
                 $log.error('url:'+ $location.path() + ', operation:' + operation + ', objectType: ' + objectType + ', description: ' + item.plainText);
-                $analytics.eventTrack('Form Validation Error', { category: objectType, label: item.$$error });
+                $analytics.eventTrack('FormSubmitAction', { category: 'Form Validation Error', label:objectType, value: item.plainText});
+
             });
             _.each(attributes, function(item) {
                 if( !_.isUndefined(item.$$error)) {
                     $log.error('url:'+ $location.path() + ', operation:' + operation + ', objectType: ' + objectType + ', attributeType: ' + item.name +  ', description: ' + item.$$error);
-                    $analytics.eventTrack('Form Validation Error', { category: objectType + '.' + item.name, label: item.$$error });
+                    $analytics.eventTrack('FormSubmitAction', { category: 'Form Validation Error', label:objectType + '.' + item.name, value: item.$$error });
                 }
             });
         }
