86c8d399191e106495c916036c850bde21b893cb
Edward Shryane <eshryane@ripe.net>
2015-10-15 16:10:54 +0200

    Fixed displaying non-ASCII latin-1 characters (e.g. e-umlaut).

--- src/main/java/net/ripe/whois/services/rest/RestClient.java
+++ src/main/java/net/ripe/whois/services/rest/RestClient.java
@@ -4,7 +4,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 public abstract class RestClient {
 
@@ -15,6 +18,7 @@ public abstract class RestClient {
         final ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
         final RestTemplate restTemplate = new RestTemplate(requestFactory);
         restTemplate.setErrorHandler(new RestResponseErrorHandler());
+        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
         return restTemplate;
     }
 }
