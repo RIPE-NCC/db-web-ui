976e7985abed6469ef3a5a9d572d5edd1ba36795
Marc Grol <mgrol@xebia.com>
2015-10-19 16:49:14 +0200

    Added html-formatted syntax for all attributes

--- src/test/javascript/spec/whoisMetaServiceSpec.js
+++ src/test/javascript/spec/whoisMetaServiceSpec.js
@@ -15,13 +15,13 @@ describe('dbWebApp: WhoisMetaService', function () {
     });
 
     it('should return correct documentation based on object- and attribute-name',function() {
-        expect($whoisMetaService.getAttributeDocumentation("mntner","admin-c")).toEqual("References an on-site administrative contact.")
-        expect($whoisMetaService.getAttributeDocumentation("inet-rtr","mp-peer")).toEqual("Details of any (interior or exterior) multiprotocol router peerings.")
+        expect($whoisMetaService.getAttributeDocumentation("mntner","admin-c")).toEqual('References an on-site administrative contact.')
+        expect($whoisMetaService.getAttributeDocumentation("inet-rtr","mp-peer")).toEqual('Details of any (interior or exterior) multiprotocol router peerings.')
     });
 
     it('should return correct syntax based on object- and attribute-name',function() {
-        expect($whoisMetaService.getAttributeSyntax("mntner","admin-c")).toEqual("TODO admin-c syntax")
-        expect($whoisMetaService.getAttributeSyntax("inet-rtr","mp-peer")).toEqual("TODO mp-peer attr of inet-rtr")
+        expect($whoisMetaService.getAttributeSyntax("mntner","admin-c")).toEqual('From 2 to 4 characters optionally followed by up to 6 digits optionally followed by a source specification. The first digit must not be "0".  Source specification starts with "-" followed by source name up to 9-character length.')
+        expect($whoisMetaService.getAttributeSyntax("inet-rtr","mp-peer")).toEqual('&lt;protocol&gt; &lt;ipv4-address&gt; &lt;options&gt;&lt;br/&gt;| &lt;protocol&gt; &lt;inet-rtr-name&gt; &lt;options&gt;&lt;br/&gt;| &lt;protocol&gt; &lt;rtr-set-name&gt; &lt;options&gt;&lt;br/&gt;| &lt;protocol&gt; &lt;peering-set-name&gt; &lt;options&gt;')
     });
 
     it('should return all objectTypes', function(){
