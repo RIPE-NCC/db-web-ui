371f5fae942d3888b7140aa80f0bfa4320a6c42b
Marc Grol <mgrol@xebia.com>
2015-12-08 16:49:54 +0100

    remove e2e tests

--- src/test/javascript/e2e/webupdatesSpec.js
+++
@@ -1,7 +0,0 @@
-//describe('Webupdates', function() {
-//    it('should have a title', function() {
-//        browser.get('https://apps.db.ripe.net/db-web-ui/');
-//
-//        expect(browser.getTitle()).toEqual('Webupdates — RIPE Network Coordination Centre');
-//    });
-//});
