8ed6ea612d4d374d1072b4d3515cb017f7b24bda
Theodoros Polychniatis <tpolychnia@ripe.net>
2015-12-14 18:51:53 +0100

    added display object

--- src/main/webapp/scripts/app/webupdates/reclaimController.js
+++ src/main/webapp/scripts/app/webupdates/reclaimController.js
@@ -3,8 +3,8 @@
 'use strict';
 
 angular.module('webUpdates')
-    .controller('ReclaimController', ['$scope', '$stateParams', '$state', '$log', 'WhoisResources', 'ModalService', 'AlertService',
-        function ($scope, $stateParams, $state, $log, WhoisResources, ModalService, AlertService) {
+    .controller('ReclaimController', ['$scope', '$stateParams', '$state', '$log', 'WhoisResources', 'ModalService', 'AlertService', 'RestService',
+        function ($scope, $stateParams, $state, $log, WhoisResources, ModalService, AlertService, RestService) {
 
             $scope.reclaim = reclaim;
 
@@ -15,14 +15,56 @@ angular.module('webUpdates')
                 AlertService.clearErrors();
 
                 // extract parameters from the url
-                $scope.source = $stateParams.source;
+                $scope.objectSource = $stateParams.source;
                 $scope.objectType = $stateParams.objectType;
                 if( !_.isUndefined($stateParams.name)) {
-                    $scope.name = decodeURIComponent($stateParams.name);
+                    $scope.objectName = decodeURIComponent($stateParams.name);
                 }
 
-                $log.debug('Url params: source:' + $scope.source + '. type:' + $scope.objectType + ', uid:' + $scope.name);
+                _fetchObject();
+
+                $log.debug('Url params: source:' + $scope.objectSource + '. type:' + $scope.objectType + ', uid:' + $scope.objectName);
             }
+
+            function _fetchObject() {
+                RestService.fetchObject($scope.objectSource, $scope.objectType, $scope.objectName, null).then(
+                    function (resp) {
+                        var whoisResources = WhoisResources.wrapWhoisResources(resp);
+                        $scope.attributes = WhoisResources.wrapAttributes(whoisResources.getAttributes());
+                        _addLinkToReferenceAttributes($scope.attributes);
+                        AlertService.populateFieldSpecificErrors($scope.objectType, $scope.attributes, resp);
+                        AlertService.setErrors(whoisResources);
+
+                    }, function (resp) {
+                        var whoisResources = WhoisResources.wrapWhoisResources(resp.data);
+                        if (!_.isUndefined(whoisResources)) {
+                            AlertService.populateFieldSpecificErrors($scope.objectType, $scope.attributes, resp.data);
+                            AlertService.setErrors(whoisResources);
+                        }
+                    }
+                );
+            }
+
+            function _addLinkToReferenceAttributes(attributes) {
+                var parser = document.createElement('a');
+                return _.map(attributes, function(attribute) {
+                    if (!_.isUndefined(attribute.link)) {
+                        attribute.link.uiHref = _displayUrl(parser, attribute);
+                    }
+                    return attribute;
+                } );
+            }
+
+            function _displayUrl(parser, attribute) {
+                parser.href = attribute.link.href;
+                var parts = parser.pathname.split('/');
+
+                return $state.href('display', {
+                    source: $scope.objectSource,
+                    objectType: attribute['referenced-type'],
+                    name: _.last(parts)
+                });
+            };
 
             function reclaim () {
                 $state.transitionTo('delete', {
