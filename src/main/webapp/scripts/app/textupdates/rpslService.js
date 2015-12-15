87bf2fbc9b250efb21b07eedeaeb7333263def10
Marc Grol <mgrol@xebia.com>
2015-12-15 21:41:40 +0100

    More rpsl-parsing more robust

--- src/main/webapp/scripts/app/textupdates/rpslService.js
+++ src/main/webapp/scripts/app/textupdates/rpslService.js
@@ -20,29 +20,84 @@ angular.module('textUpdates')
         };
 
         this.fromRpsl = function (rpslText) {
-            var attrs = [];
-            _.each(rpslText.split('\n'), function (item) {
-                var splitted = item.split(':');
-                if (splitted.length > 0 && !_.isUndefined(splitted[0]) && !_.isEmpty(_.trim(splitted[0]))) {
-                    var key = _.trim(splitted[0]);
+            var rawAttributes = _chopInParts(rpslText);
 
-                    var value = undefined;
-                    var comment = undefined;
-                    if (!_.isUndefined(splitted[1]) && !_.isEmpty(_.trim(splitted[1]))) {
-                        var valueSpiltted = splitted[1].split('#');
+            var jsonAttributes = [];
+            _.each(rawAttributes, function (rawAttribute) {
+                var key = undefined;
+                var value = '';
+                var comment = '';
 
-                        if (!_.isUndefined(valueSpiltted[0]) && !_.isEmpty(_.trim(valueSpiltted[0]))) {
-                            value = _.trim(valueSpiltted[0]);
-                        }
-                        if (!_.isUndefined(valueSpiltted[1]) && !_.isEmpty(_.trim(valueSpiltted[1]))) {
-                            comment = _.trim(valueSpiltted[1]);
-                        }
+                var keyWithRest = rawAttribute.split(':');
+                if (keyWithRest.length > 0 && !_.isEmpty(_.trim(keyWithRest[0]))) {
+                    key = _.trim(keyWithRest[0]);
+
+                    if (keyWithRest.length > 1) {
+                        _.each(keyWithRest[1].split('\n'), function (item) {
+                            var trimmed = _.trimRight(item);
+
+                            if (trimmed.indexOf('#') < 0) {
+                                // no comment
+                                value = value.concat(trimmed);
+                            } else if (_.startsWith(trimmed, '#')) {
+                                // only comment
+                                comment = comment.concat(trimmed.substring(1));
+                            } else {
+                                var valueWithRest = trimmed.split('#');
+                                value = value.concat(valueWithRest[0]);
+                                comment = comment.concat(valueWithRest[1]);
+                            }
+                        });
                     }
-                    attrs.push({name: key, value: value, comment:comment});
+
+                    jsonAttributes.push({
+                        name: key,
+                        value: _noEmptyValues(value),
+                        comment: _noEmptyValues(comment)
+                    });
                 }
             });
-            return attrs;
+            return jsonAttributes;
         };
+
+
+        function _chopInParts(rpslText) {
+            var records = [];
+
+            var record = '';
+            for (var idx = 0; idx < rpslText.length; idx++) {
+                var current = rpslText.charAt(idx);
+                var next = rpslText.charAt(idx + 1);
+
+                record += current;
+
+                // newline followed by alpha-numeric character is attribute separator
+                if (idx === rpslText.length - 1 || (current === '\n' && _isLetter(next))) {
+                    // end of attribute reached
+                    records.push(_.cloneDeep(record));
+                    record = '';
+                }
+            }
+            return records;
+        }
+
+        function _isLetter(c) {
+            if (c === '') {
+                return false;
+            }
+            return c.toLowerCase() != c.toUpperCase();
+        }
+
+        function _noEmptyValues(value) {
+            if (_.isUndefined(value)) {
+                return undefined;
+            }
+            var trimmed = _.trim(value);
+            if (_.isEmpty(trimmed)) {
+                return undefined;
+            }
+            return trimmed;
+        }
 
     }]);
 
--- src/test/javascript/spec/textupdates/rpslServiceSpec.js
+++ src/test/javascript/spec/textupdates/rpslServiceSpec.js
@@ -31,25 +31,135 @@ describe('textUpdates: RpslService', function () {
     });
 
     it('should convert multi-line json-attributes with comments into rpsl', function () {
-        // TODO
+        // TODO: can this happen?
     });
 
-    it('should parse regular rpsl into json-attributes', function () {
-        var rpsl = 'person:        \n' +
+    it('should parse regular rpsl with comments into json-attributes', function () {
+        var rpsl =
+            'person:        \n' +
             'phone:         +316\n' +
             'address:       Singel # My comment\n';
 
         var attrs = $RpslService.fromRpsl(rpsl);
 
-        expect(attrs).toEqual( [
-            {name: 'person',  value: undefined, comment: undefined},
-            {name: 'phone',   value: '+316',    comment: undefined},
-            {name: 'address', value: 'Singel',  comment: 'My comment' }
+        expect(attrs).toEqual([
+            {name: 'person', value: undefined, comment: undefined},
+            {name: 'phone', value: '+316', comment: undefined},
+            {name: 'address', value: 'Singel', comment: 'My comment'}
         ]);
     });
 
-    it('should parse multi-line rpsl into json-attributes', function () {
-        // TODO
+    it('should parse rpsl with spaces before colon', function () {
+        var rpsl = 'person    :   Me     #hoi\n';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: 'Me', comment: 'hoi'}
+        ]);
     });
 
+    it('should parse empty value', function () {
+        var rpsl = 'person:  \n';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: undefined, comment: undefined}
+        ]);
+    });
+
+    it('should parse empty value with comment', function () {
+        // TODO: is this allowed?
+        var rpsl = 'person: # A comment';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: undefined, comment: 'A comment'}
+        ]);
+    });
+
+
+    it('should ignore empty attribute without keye', function () {
+        var rpsl = ': value # comment';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([]);
+    });
+
+    it('should ignore empty lines in rpsl', function () {
+        // TODO should we?
+        var rpsl =
+            'person:        #hoi\n' +
+            'phone:         +316\n' +
+            '\n' +
+            'address:       Singel # My comment\n';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: undefined, comment: 'hoi'},
+            {name: 'phone', value: '+316', comment: undefined},
+            {name: 'address', value: 'Singel', comment: 'My comment'}
+        ]);
+    });
+
+    it('should parse value continuation with space', function () {
+        var rpsl =
+            'person: value  1# comment 1\n' +
+            ' more value 2 # and more comment';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: 'value  1 more value 2', comment: 'comment 1 and more comment'}
+        ]);
+    });
+
+    it('should parse value continuation with tab', function () {
+        var rpsl =
+            'person: value  1\n' +
+            '\tmore value 2 # more comment';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: 'value  1\tmore value 2', comment: 'more comment'}
+        ]);
+    });
+
+    it('should parse value continuation with plus', function () {
+        // TODO: is this ok?
+        var rpsl =
+            'person: value  1\n' +
+            '+more value 2 # second comment';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: 'value  1+more value 2', comment: 'second comment'}
+        ]);
+    });
+
+    it('should parse multi-line rpsl with comments into json-attributes', function () {
+        var rpsl =
+            'person:        \n' +
+            'address:       Singel # part 1\n' +
+            '   Amsterdam # part 2\n' +
+            '\tNederland\n' +
+            '++  # part 4\n' +
+            '++Europa\n' +
+            'phone:         +316 #ok\n';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual([
+            {name: 'person', value: undefined, comment: undefined},
+            {name: 'address', value: 'Singel    Amsterdam \tNederland++  ++Europa', comment: 'part 1 part 2 part 4'},
+            {name: 'phone', value: '+316', comment: 'ok'},
+        ]);
+    });
 });
+
