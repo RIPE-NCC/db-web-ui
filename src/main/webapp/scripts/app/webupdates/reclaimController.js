2b13d1026b5b35735de65f269ccca5d9215737bd
Theodoros Polychniatis <tpolychnia@ripe.net>
2015-12-15 15:00:27 +0100

    added unit tests for reclaim

--- src/main/webapp/scripts/app/webupdates/reclaimController.js
+++ src/main/webapp/scripts/app/webupdates/reclaimController.js
@@ -8,7 +8,6 @@ angular.module('webUpdates')
 
             $scope.reclaim = reclaim;
 
-
             _initialisePage();
 
             function _initialisePage() {
--- src/test/javascript/spec/webupdates/reclaimControllerSpec.js
+++ src/test/javascript/spec/webupdates/reclaimControllerSpec.js
@@ -9,6 +9,13 @@ describe('webUpdates: ReclaimController', function () {
     var $rootScope;
     var $log;
 
+    var INETNUM='111 - 255';
+    var SOURCE='RIPE';
+
+    var createReclaimController;
+
+    var objectToDisplay;
+
     beforeEach(function () {
         module('webUpdates');
 
@@ -36,8 +43,46 @@ describe('webUpdates: ReclaimController', function () {
                 }
             };
 
-            $httpBackend.whenGET(/.*.html/).respond(200);
-            $httpBackend.flush();
+            objectToDisplay = WhoisResources.wrapWhoisResources(
+                {
+                    objects: {
+                        object: [
+                            {
+                                'primary-key': {attribute: [{name: 'inetnum', value: INETNUM}]},
+                                attributes: {
+                                    attribute: [
+                                        {name: 'inetnum', value: INETNUM},
+                                        {name: 'mnt-by', value: 'TEST-MNT'},
+                                        {name: 'descr', value: 'description'},
+                                        {name: 'source', value: 'RIPE'}
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+
+                });
+
+            createReclaimController = function () {
+
+                $httpBackend.whenGET(/.*.html/).respond(200);
+
+                $httpBackend.expectGET('api/whois/RIPE/inetnum/111%20-%20255?unfiltered=true').respond(function(method,url) {
+                    return [200, objectToDisplay, {}];
+                });
+
+                $stateParams.source = SOURCE;
+                $stateParams.objectType = 'inetnum';
+                $stateParams.name = '111%20-%20255';
+
+                _$controller_('ReclaimController', {
+                    $scope: $scope, $state: $state, $stateParams: $stateParams
+                });
+
+                $httpBackend.flush();
+            };
+
+
         });
     });
 
@@ -46,8 +91,34 @@ describe('webUpdates: ReclaimController', function () {
         $httpBackend.verifyNoOutstandingRequest();
     });
 
-    it('should just work'), function () {
-        expect(true).toBe(true);
-    };
+    it('should get parameters from url', function () {
+
+        createReclaimController();
+
+        expect($scope.objectType).toBe('inetnum');
+        expect($scope.objectSource).toBe(SOURCE);
+        expect($scope.objectName).toBe(INETNUM);
+    });
+
+    it('should go to delete controler on reclaim', function() {
+
+        createReclaimController();
+
+        $scope.reclaim();
+        $httpBackend.flush();
+
+        expect($state.current.name).toBe('delete');
+        expect($stateParams.onCancel).toBe('reclaim');
+    });
+
+    it('should populate the ui with attributes', function () {
+        var stateBefore = $state.current.name;
+
+        createReclaimController();
+
+        expect($scope.attributes.getSingleAttributeOnName('inetnum').value).toBe(INETNUM);
+        expect($scope.attributes.getSingleAttributeOnName('descr').value).toEqual('description');
+        expect($scope.attributes.getSingleAttributeOnName('source').value).toEqual(SOURCE);
+    });
 
 });
