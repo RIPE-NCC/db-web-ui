8db7de9568f078537683514e58bfee977508e64f
Marc Grol <mgrol@xebia.com>
2015-12-15 17:42:39 +0100

    Added test-cases for rpsl parsing

--- src/main/webapp/scripts/app/textupdates/rpslService.js
+++ src/main/webapp/scripts/app/textupdates/rpslService.js
@@ -11,6 +11,9 @@ angular.module('textUpdates')
                 if (!_.isUndefined(item.value)) {
                     rpslData = rpslData.concat(item.value);
                 }
+                if (!_.isUndefined(item.comment)) {
+                    rpslData = rpslData.concat(' # ' + item.comment);
+                }
                 rpslData = rpslData.concat('\n');
             });
             return rpslData;
@@ -21,11 +24,21 @@ angular.module('textUpdates')
             _.each(rpslText.split('\n'), function (item) {
                 var splitted = item.split(':');
                 if (splitted.length > 0 && !_.isUndefined(splitted[0]) && !_.isEmpty(_.trim(splitted[0]))) {
-                    var valueWithComment = undefined;
+                    var key = _.trim(splitted[0]);
+
+                    var value = undefined;
+                    var comment = undefined;
                     if (!_.isUndefined(splitted[1]) && !_.isEmpty(_.trim(splitted[1]))) {
-                        valueWithComment = _.trim(splitted[1]);
+                        var valueSpiltted = splitted[1].split('#');
+
+                        if (!_.isUndefined(valueSpiltted[0]) && !_.isEmpty(_.trim(valueSpiltted[0]))) {
+                            value = _.trim(valueSpiltted[0]);
+                        }
+                        if (!_.isUndefined(valueSpiltted[1]) && !_.isEmpty(_.trim(valueSpiltted[1]))) {
+                            comment = _.trim(valueSpiltted[1]);
+                        }
                     }
-                    attrs.push({name: _.trim(splitted[0]), value: valueWithComment});
+                    attrs.push({name: key, value: value, comment:comment});
                 }
             });
             return attrs;
--- src/test/javascript/spec/textupdates/rpslServiceSpec.js
+++ src/test/javascript/spec/textupdates/rpslServiceSpec.js
@@ -15,23 +15,40 @@ describe('textUpdates: RpslService', function () {
     });
 
     it('should convert regular json-attributes into rpsl', function () {
-        // TODO
+        var attrs = [
+            {name: 'person'},
+            {name: 'phone', value: '+316'},
+            {name: 'address', value: 'Singel', comment: 'My comment'}
+
+        ];
+        var rpsl = $RpslService.toRpsl(attrs);
+
+        expect(rpsl).toEqual(
+            'person:        \n' +
+            'phone:         +316\n' +
+            'address:       Singel # My comment\n'
+        );
     });
 
-    it('should convert json-attributes with comments into rpsl', function () {
+    it('should convert multi-line json-attributes with comments into rpsl', function () {
         // TODO
     });
-
 
     it('should parse regular rpsl into json-attributes', function () {
-        // TODO
+        var rpsl = 'person:        \n' +
+            'phone:         +316\n' +
+            'address:       Singel # My comment\n';
+
+        var attrs = $RpslService.fromRpsl(rpsl);
+
+        expect(attrs).toEqual( [
+            {name: 'person',  value: undefined, comment: undefined},
+            {name: 'phone',   value: '+316',    comment: undefined},
+            {name: 'address', value: 'Singel',  comment: 'My comment' }
+        ]);
     });
 
     it('should parse multi-line rpsl into json-attributes', function () {
-        // TODO
-    });
-
-    it('should parse rpsl with comments into json-attributes', function () {
         // TODO
     });
 
