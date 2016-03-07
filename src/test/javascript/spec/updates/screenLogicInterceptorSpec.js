ef87a278f73461239e4f042a038c01c035171df3
Thiago da Cruz <tdacruzper@ripe.net>
2016-03-07 16:04:45 +0100

    Add tests for disableOrgWhenStatusIsAssignedPI

--- src/test/javascript/spec/updates/screenLogicInterceptorSpec.js
+++ src/test/javascript/spec/updates/screenLogicInterceptorSpec.js
@@ -26,6 +26,40 @@ describe('updates: ScreenLogicInterceptor', function () {
         );
     }
 
+    it('should disable org attribute from inet6num when status is ASSIGNED PI', function() {
+
+        var inet6NumSubject = _wrap('inet6num', inet6NumAttributes);
+        inet6NumSubject.setSingleAttributeOnName('status', 'ASSIGNED PI');
+
+        var attributes = interceptor.beforeEdit('Modify', 'RIPE', 'inet6num', inet6NumSubject);
+
+        var orgAttr = attributes.getSingleAttributeOnName('org');
+        expect(orgAttr.$$disable).toBe(true);
+    });
+
+    it('should disable org attribute from inetnum when status is ASSIGNED PI', function() {
+
+        var inetNumSubject = _wrap('inetnum', inetNumAttributes);
+        inetNumSubject.setSingleAttributeOnName('status', 'ASSIGNED PI');
+
+        var attributes = interceptor.beforeEdit('Modify', 'RIPE', 'inetnum', inetNumSubject);
+
+        var orgAttr = attributes.getSingleAttributeOnName('org');
+        expect(orgAttr.$$disable).toBe(true);
+    });
+
+    it('should disable org attribute from aut-num when status is ASSIGNED PI', function() {
+
+        var autNumSubject = _wrap('aut-num', autNumAttributes);
+        autNumSubject.setSingleAttributeOnName('status', 'ASSIGNED PI');
+
+        var attributes = interceptor.beforeEdit('Modify', 'RIPE', 'aut-num', autNumSubject);
+
+        var orgAttr = attributes.getSingleAttributeOnName('org');
+        console.log(JSON.stringify(orgAttr));
+        expect(orgAttr.$$disable).toBe(true);
+    });
+
     it('should not remove sponsoring org from inet6num addable attributes when status is LEGACY', function() {
 
         var inet6NumSubject = _wrap('inet6num', inet6NumAttributes);
@@ -247,6 +281,26 @@ describe('updates: ScreenLogicInterceptor', function () {
     }, {
         name : 'last-modified',
         value : '2016-03-06T10:58:07Z'
+    }, {
+        name : 'source',
+        value : 'RIPE'
+    } ];
+
+    var autNumAttributes =  [{
+        name : 'aut-num',
+        value : 'AS123'
+    }, {
+        name : 'as-name',
+        value : 'EXAMPLE'
+    }, {
+        name : 'status',
+        value : 'ASSIGNED PI'
+    }, {
+        name : 'org',
+        value : 'ORG-Fr4-RIPE'
+    }, {
+        name : 'mnt-by',
+        value : 'RIPE-NCC-END-MNT'
     }, {
         name : 'source',
         value : 'RIPE'
