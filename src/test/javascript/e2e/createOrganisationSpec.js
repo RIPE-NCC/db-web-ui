4ce7e80fd8cb7fff160cd98348b5cd591619604d
Etch on Mac <paul@pauletchells.com>
2016-04-08 14:03:06 +0200

    added more checks for org attributes

--- src/test/javascript/e2e/createOrganisationSpec.js
+++ src/test/javascript/e2e/createOrganisationSpec.js
@@ -1,7 +1,7 @@
 /*global beforeEach, browser, describe, expect, it, require */
 var page = require('./homePageObject');
 
-describe('webupdates homepage', function() {
+describe('The organisation editor', function() {
 
     beforeEach(function() {
         browser.get('/#/webupdates/create/RIPE/organisation');
@@ -11,7 +11,7 @@ describe('webupdates homepage', function() {
         // });
     });
 
-    it('should not crash when showing index page', function() {
+    it('should not crash when showing the single line editor', function() {
         expect(page.searchTextInput.isPresent()).toEqual(true);
         expect(page.selectMaintainerDropdown.isPresent()).toEqual(true);
         // test that we're detecting failures properly -- ptor gets confused by bad configs so make sure we're not using
@@ -26,19 +26,39 @@ describe('webupdates homepage', function() {
         page.btnSwitchToWebCreate.click();
     });
 
-    it('should have the right attributes for an organisation', function() {
+    it('should have the right attributes', function() {
         expect(page.inpOrgName.isPresent()).toEqual(true);
-        expect(page.inpOrganisation.isPresent()).toEqual(true);
-        expect(page.inpOrgType.isPresent()).toEqual(true);
-        expect(page.inpAddress.isPresent()).toEqual(true);
-        expect(page.inpEmail.isPresent()).toEqual(true);
-        expect(page.inpAbuseC.isPresent()).toEqual(true);
-        expect(page.inpMntRef.isPresent()).toEqual(true);
-        expect(page.inpSource.isPresent()).toEqual(true);
+        expect(page.inpOrgName.getAttribute('disabled')).toBeFalsy();
 
-        expect(page.inpOrgType.getAttribute('disabled')).toEqual('true');
-        expect(page.inpSource.getAttribute('disabled')).toEqual('true');
+        expect(page.inpOrganisation.isPresent()).toEqual(true);
+        expect(page.inpOrganisation.getAttribute('disabled')).toBeFalsy();
         expect(page.inpOrganisation.getAttribute('value')).toEqual('AUTO-1');
+
+        expect(page.inpOrgType.isPresent()).toEqual(true);
+        expect(page.inpOrgType.getAttribute('disabled')).toBeTruthy();
+
+        expect(page.inpAddress.isPresent()).toEqual(true);
+        expect(page.inpAddress.getAttribute('disabled')).toBeTruthy();
+
+        expect(page.inpEmail.isPresent()).toEqual(true);
+        expect(page.inpEmail.getAttribute('disabled')).toBeTruthy();
+
+        expect(page.inpAbuseC.isPresent()).toEqual(true);
+        expect(page.inpAbuseC.getAttribute('disabled')).toBeTruthy();
+
+        expect(page.inpMntRef.isPresent()).toEqual(true);
+        expect(page.inpMntRef.getAttribute('disabled')).toBeFalsy();
+
+        expect(page.inpSource.isPresent()).toEqual(true);
+        expect(page.inpSource.getAttribute('disabled')).toBeTruthy();
+    });
+
+    xit('should validate input fields', function() {
+        // TODO: test field validation
+    });
+
+    xit('should work in single line mode', function() {
+        // TODO: create an org in single line mode
     });
 
 });
