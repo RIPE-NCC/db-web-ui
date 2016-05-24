22259f38cbb92795c22a17854dba12bf92671aa4
Macbook Etchells <paul@pauletchells.com>
2016-05-24 15:56:45 +0200

    Fixed mocks for testing inet(6)num

--- src/test/javascript/e2e/navigateToObjectCreationSpec.js
+++ src/test/javascript/e2e/navigateToObjectCreationSpec.js
@@ -52,46 +52,34 @@ describe('webupdates homepage', function() {
         expect(page.heading.getText()).toEqual('Create "filter-set" object');
     });
 
-    it('should show an editor for inet6num', function() {
+    fit('should show an editor for inet6num', function() {
         expect(page.selectForm.isPresent()).toEqual(true);
         page.selectObjectType('inet6num').click();
         page.btnNavigateToCreate.click();
         expect(page.createForm.isPresent()).toEqual(true);
         expect(page.heading.getText()).toEqual('Create "inet6num" object');
-        page.inpInet6num.sendKeys('2001:888:2000::/36');
+        page.inpInet6num.sendKeys('2001:888:2000::/38');
         page.scrollIntoView(page.inpStatusLink); // bring 'status' into view
         page.inpStatusLink.click(); // click on dropdown to populate it.
-        expect(page.inpStatusList.count()).toBe(6);
+        expect(page.inpStatusList.count()).toBe(2);
         expect(page.inpStatusList.get(0).getText()).toEqual('AGGREGATED-BY-LIR');
-        expect(page.inpStatusList.get(1).getText()).toEqual('ALLOCATED-BY-LIR');
-        expect(page.inpStatusList.get(2).getText()).toEqual('ALLOCATED-BY-RIR');
-        expect(page.inpStatusList.get(3).getText()).toEqual('ASSIGNED');
-        expect(page.inpStatusList.get(4).getText()).toEqual('ASSIGNED ANYCAST');
-        expect(page.inpStatusList.get(5).getText()).toEqual('ASSIGNED PI');
+        expect(page.inpStatusList.get(1).getText()).toEqual('ASSIGNED');
     });
 
-    it('should show an editor for inetnum', function() {
+    fit('should show an editor for inetnum', function() {
         expect(page.selectForm.isPresent()).toEqual(true);
         page.selectObjectType('inetnum').click();
         page.btnNavigateToCreate.click();
         expect(page.createForm.isPresent()).toEqual(true);
         expect(page.heading.getText()).toEqual('Create "inetnum" object');
         expect(page.inpStatusLink.getText()).toEqual('Specifies the kind of resource.');
-        //page.scrollIntoView(page.inpStatusLink); // let's have a look at that link
-        //page.inpStatusLink.click(); // click on dropdown to populate it.
-        //expect(page.inpStatus.isPresent()).toEqual(true);
-        //page.inpInetnum.click();
-        //page.inpInetnum.sendKeys('193.0.4.0 - 193.0.4.255');
-        //page.scrollIntoView(page.inpStatusLink); // let's have a look at that link
-        //page.inpStatusLink.click(); // click on dropdown to populate it.
-        //expect(page.inpStatusList.count()).toBe(0);
-        //expect(page.inpStatusList.get(0).getText()).toEqual('ALLOCATED PA');
-        //
         page.inpInetnum.sendKeys('213.159.160.0 - 213.159.190.255');
         page.scrollIntoView(page.inpStatusLink); // let's have a look at that link
         page.inpStatusLink.click(); // click on dropdown to populate it.
         expect(page.inpStatusList.count()).toBe(3);
         expect(page.inpStatusList.get(0).getText()).toEqual('ASSIGNED PA');
+        expect(page.inpStatusList.get(1).getText()).toEqual('LIR-PARTITIONED PA');
+        expect(page.inpStatusList.get(2).getText()).toEqual('SUB-ALLOCATED PA');
     });
 
     it('should show an editor for inet-rtr', function() {
