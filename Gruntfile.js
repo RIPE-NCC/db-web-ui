23a43377db9a5b563992ef10dca08df931eb6f3b
Paul Etchells <etch@dhcp-21-244.ripe.net>
2016-08-29 17:42:37 +0200

    fixed mocking -- dno how that got broken :\

--- Gruntfile.js
+++ Gruntfile.js
@@ -575,7 +575,6 @@ module.exports = function (grunt) {
 
                         function substituteText(m, filename) {
                             keeplooping = true;
-                            console.log('lookin for ' + dir + '/' + filename);
                             return fs.readFileSync(dir + '/' + filename).toString();
                         }
 
