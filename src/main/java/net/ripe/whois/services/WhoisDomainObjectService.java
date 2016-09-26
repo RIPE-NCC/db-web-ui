c2a35a3a500ce753ade35be3fa27404f000d5772
Paul Etchells <etch@dhcp-21-244.ripe.net>
2016-09-26 15:05:32 +0200

    not using spring to configure jaxrs

--- src/main/java/net/ripe/whois/services/WhoisDomainObjectService.java
+++ src/main/java/net/ripe/whois/services/WhoisDomainObjectService.java
@@ -1,9 +1,12 @@
 package net.ripe.whois.services;
 
-import com.google.common.collect.Maps;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import net.ripe.db.whois.api.rest.domain.WhoisObject;
 import net.ripe.db.whois.api.rest.domain.WhoisResources;
-import net.ripe.whois.services.rest.RestClient;
+import org.glassfish.jersey.client.ClientResponse;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,45 +15,52 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.util.UriTemplate;
 
-import java.net.URI;
-import java.util.HashMap;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
-public class WhoisDomainObjectService extends RestClient {
+public class WhoisDomainObjectService {
     private static final Logger LOGGER = LoggerFactory.getLogger(WhoisDomainObjectService.class);
 
     private String domainObjectApiUrl;
 
-    @Autowired
-    public WhoisDomainObjectService(@Value("${rest.api.ripeUrl}") final String ripeUrl) {
-        this.domainObjectApiUrl = ripeUrl + "/whois/domain-objects";
+    private static final Client client;
+
+    static {
+        final JacksonJsonProvider jsonProvider = new JacksonJaxbJsonProvider()
+                .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false)
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+        client = ClientBuilder.newBuilder()
+                .register(MultiPartFeature.class)
+                .register(jsonProvider)
+                .build();
     }
 
-    public ResponseEntity<WhoisResources> createDomainObjects(final String source, final List<WhoisObject> domainObjects, final HttpHeaders headers) {
-        final String url = "{url}/{source}";
+    @Autowired
+    public WhoisDomainObjectService(@Value("${rest.api.ripeUrl}") final String ripeUrl) {
+        this.domainObjectApiUrl = ripeUrl;
+    }
 
-        final HashMap<String, Object> variables = Maps.newHashMap();
-        variables.put("url", domainObjectApiUrl);
-        variables.put("source", source);
-
-        URI uri = new UriTemplate(url).expand(variables);
+    public ResponseEntity<WhoisResources> createDomainObjects(final String source, String[] passwords, final List<WhoisObject> domainObjects, final HttpHeaders headers) {
 
         WhoisResources whoisResources = new WhoisResources();
         whoisResources.setWhoisObjects(domainObjects);
 
-        LOGGER.debug("Performing create {}", uri.toString());
-
-        WhoisResources result = null;
-        try {
-            result = restTemplate.postForObject(uri + "?password=XS4ALL", whoisResources, WhoisResources.class);
-            return new ResponseEntity<>(result, HttpStatus.CREATED);
-        } catch (HttpClientErrorException e) {
-            return new ResponseEntity<>(result, e.getStatusCode());
+        String passwdQueryString = "";
+        if (passwords != null && passwords.length > 0) {
+            passwdQueryString = "?password=" + String.join("&password=", Arrays.asList(passwords));
         }
+        WebTarget target = client.target(domainObjectApiUrl).path("/whois/domain-objects/" + source + passwdQueryString);
+        Invocation.Builder builder = target.request();
+        builder.header(HttpHeaders.COOKIE, headers.get(HttpHeaders.COOKIE));
+        builder.header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_TYPE);
+        ClientResponse response = builder.post(Entity.entity(whoisResources, MediaType.APPLICATION_JSON_TYPE), ClientResponse.class);
+        LOGGER.info("Whois responded with status: " + response.getStatus());
+        return new ResponseEntity<>((WhoisResources) response.getEntity(), HttpStatus.CREATED);
     }
 
 }
