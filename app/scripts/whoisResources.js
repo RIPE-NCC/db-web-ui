032b16751637ef304efb209944c350eeb39ab323
Edward Shryane <eshryane@ripe.net>
2016-10-19 11:56:20 +0200

    Removed logging

--- app/scripts/whoisResources.js
+++ app/scripts/whoisResources.js
@@ -46,7 +46,6 @@ angular.module('dbWebApp')
                     }
                 });
             });
-            $log.info('attrsToFilterOn:'+attrsToFilterOn);
             return attrsToFilterOn;
         };
 
