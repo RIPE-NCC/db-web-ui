66079c8dd0d91d900e34baa21e6c0257be5fdb1e
Paul Etchells <etch@dhcp-21-176.ripe.net>
2016-11-09 16:21:10 +0100

    more expectations are expected

--- frontend/test/javascript/e2e/createDomainSpec.js
+++ frontend/test/javascript/e2e/createDomainSpec.js
@@ -32,29 +32,29 @@ describe('The domain wizard', function () {
             return browser.isElementPresent(page.inpNserver1);
         }, 5000);
         page.inpNserver1.sendKeys('ns1.xs4all.nl');
-        // page.inpNserver2.sendKeys('nsXXX.xs4all.nl');
-        // var liContainer = page.inpNserver2.element(by.xpath('..'));
-        //
-        // browser.wait(function() {
-        //     return browser.isElementPresent(liContainer.element(by.css('.text-error')));
-        // }, 5000);
-        //
-        // expect(liContainer.getAttribute('class')).toContain('has-error');
-        // expect(page.inpAdminC4.isDisplayed()).toEqual(false);
-        // expect(page.inpTechC5.isDisplayed()).toEqual(false);
-        // expect(page.inpZoneC6.isDisplayed()).toEqual(false);
-        //
-        // page.inpNserver2.clear();
-        // page.inpNserver2.sendKeys('ns2.xs4all.nl');
-        // browser.wait(function() {
-        //     return browser.isElementPresent(liContainer.element(by.css('.text-info')));
-        // }, 5000);
-        //
-        // expect(liContainer.getAttribute('class')).not.toContain('has-error');
-        // expect(page.inpReverseZoneTable.all(by.css('tbody tr')).count()).toEqual(128);
-        // expect(page.inpAdminC4.isDisplayed()).toEqual(true);
-        // expect(page.inpTechC5.isDisplayed()).toEqual(true);
-        // expect(page.inpZoneC6.isDisplayed()).toEqual(true);
+        page.inpNserver2.sendKeys('nsXXX.xs4all.nl');
+        var liContainer = page.inpNserver2.element(by.xpath('..'));
+
+        browser.wait(function() {
+            return browser.isElementPresent(liContainer.element(by.css('.text-error')));
+        }, 5000);
+
+        expect(liContainer.getAttribute('class')).toContain('has-error');
+        expect(page.inpAdminC4.isDisplayed()).toEqual(false);
+        expect(page.inpTechC5.isDisplayed()).toEqual(false);
+        expect(page.inpZoneC6.isDisplayed()).toEqual(false);
+
+        page.inpNserver2.clear();
+        page.inpNserver2.sendKeys('ns2.xs4all.nl');
+        browser.wait(function() {
+            return browser.isElementPresent(liContainer.element(by.css('.text-info')));
+        }, 5000);
+
+        expect(liContainer.getAttribute('class')).not.toContain('has-error');
+        expect(page.inpReverseZoneTable.all(by.css('tbody tr')).count()).toEqual(128);
+        expect(page.inpAdminC4.isDisplayed()).toEqual(true);
+        expect(page.inpTechC5.isDisplayed()).toEqual(true);
+        expect(page.inpZoneC6.isDisplayed()).toEqual(true);
     });
 
 
