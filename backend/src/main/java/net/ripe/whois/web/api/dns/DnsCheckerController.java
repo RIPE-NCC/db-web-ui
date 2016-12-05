5ec5835a064e3b1a25ebe55d25b29805bae76955
Thiago da Cruz <tdacruzper@ripe.net>
2016-12-05 11:48:23 +0100

    Add new DNS check for the name server

--- backend/src/main/java/net/ripe/whois/web/api/dns/DnsCheckerController.java
+++ backend/src/main/java/net/ripe/whois/web/api/dns/DnsCheckerController.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -14,69 +12,83 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.xbill.DNS.Lookup;
+import org.xbill.DNS.Resolver;
+import org.xbill.DNS.SimpleResolver;
+import org.xbill.DNS.TextParseException;
+import org.xbill.DNS.Type;
 
-import javax.ws.rs.ClientErrorException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/api/dns")
 public class DnsCheckerController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DnsCheckerController.class);
 
-    private final RestTemplate restTemplate;
+    private final CrowdClient crowdClient;
+    private final int port;
 
     @Autowired
-    public DnsCheckerController(
-            @Value("${dns.checker.url}") final String dnsCheckerUrl,
-            final CrowdClient crowdClient,
-            final RestTemplate restTemplate) {
-        this.dnsCheckerUrl = dnsCheckerUrl;
+    public DnsCheckerController(final CrowdClient crowdClient, @Value("${dns.check.port:53}") final int port) {
         this.crowdClient = crowdClient;
-        this.restTemplate = restTemplate;
+        this.port = port;
     }
 
-    /**
-     * This controller calls the Zonemaster service to validate a nameserver address.
-     *
-     * The get_ns_ips check returns all IP addresses for a given name.
-     *
-     * Ref: https://github.com/dotse/zonemaster-backend/blob/master/docs/API.md#json-rpc-call-2-get_ns_ips
-     */
     @RequestMapping(value = "/status", method = RequestMethod.GET)
-    public ResponseEntity<String> status(
-            @CookieValue(value = "crowd.token_key") final String crowdToken,
-            @RequestParam(value = "ns") final String nameserver) {
+    public ResponseEntity<String> status(@CookieValue(value = "crowd.token_key") final String crowdToken,
+                                         @RequestParam(value = "ns") final String ns,
+                                         @RequestParam(value = "record") final String record) {
 
         if (crowdToken == null || !crowdClient.getUserSession(crowdToken).isActive()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        final String request = String.format("{\"method\": \"get_ns_ips\", \"params\": \"%s\"}", nameserver);
-        LOGGER.debug("Checking status for " + nameserver);
         try {
-            final ResponseEntity<String> response = restTemplate.exchange(
-                    dnsCheckerUrl,
-                    HttpMethod.POST,
-                    new HttpEntity<>(request),
-                    String.class);
+            final InetAddress[] addresses = InetAddress.getAllByName(ns);
 
-            if (response.getBody().contains("0.0.0.0")) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            for (InetAddress address : addresses) {
+
+                final Resolver udpResolver = getResolver(address, port, false, 5);
+                final Lookup udpLookup = executeQuery(record, udpResolver);
+                if (udpLookup.getAnswers() == null || udpLookup.getAnswers().length == 0){
+                    return new ResponseEntity(getErrorMessage(ns, address.getHostAddress(), port, udpLookup.getResult(), "UDP"), HttpStatus.OK);
+                }
+
+                final Resolver tcpResolver = getResolver(address, port, true, 10);
+                final Lookup tcpLookup = executeQuery(record, tcpResolver);
+                if (tcpLookup.getAnswers() == null || tcpLookup.getAnswers().length == 0){
+                    return new ResponseEntity(getErrorMessage(ns, address.getHostAddress(), port, tcpLookup.getResult(), "TCP"), HttpStatus.OK);
+                }
+
             }
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (ClientErrorException e) {
-            LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<>(HttpStatus.valueOf(e.getResponse().getStatus()));
-        } catch (HttpClientErrorException e) {
-            LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<>(e.getStatusCode());
-        } catch (RuntimeException e) {
-            LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            LOGGER.info("Could not test DNS for "+ns+" "+record );
+            LOGGER.info(e.getMessage(), e);
+            return new ResponseEntity("{code: -1, message:'Could not query "+ns+"'}", HttpStatus.OK);
         }
+
+        return new ResponseEntity("{code: 0, message:'Name server looks ok'}", HttpStatus.OK);
+
     }
 
-    private final CrowdClient crowdClient;
-    private final String dnsCheckerUrl;
-    private final static Logger LOGGER = LoggerFactory.getLogger(DnsCheckerController.class);
+    private String getErrorMessage(final String ns, final String address, final int port, final int lookupResult, final String protocol) {
+        return "{code: "+lookupResult+", message:'Could not query "+ns+" ("+address+") using "+protocol+" on port "+port+"'}";
+    }
+
+    private Lookup executeQuery(final String record, final Resolver resolver) throws TextParseException {
+        final Lookup lookup = new Lookup(record, Type.SOA);
+        lookup.setResolver(resolver);
+        lookup.run();
+
+        return lookup;
+    }
+
+    private Resolver getResolver(final InetAddress address, final int port, final boolean useTCP, final int timeOut) throws UnknownHostException {
+        final SimpleResolver resolver = new SimpleResolver(address.getHostAddress());
+        resolver.setTCP(useTCP);
+        resolver.setPort(port);
+        resolver.setTimeout(timeOut);
+        return resolver;
+    }
 }
