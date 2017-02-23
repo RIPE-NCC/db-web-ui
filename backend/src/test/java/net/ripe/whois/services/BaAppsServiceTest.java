7899e097c0a2371e04c21204bee2aed9b884ffa6
Mikhail Puzanov <mpuzanov@ripe.net>
2017-02-23 13:00:30 +0100

    Add some meaningful tests for the ba-apps proxying service.

--- backend/src/test/java/net/ripe/whois/services/BaAppsServiceTest.java
+++ backend/src/test/java/net/ripe/whois/services/BaAppsServiceTest.java
@@ -1,5 +1,7 @@
 package net.ripe.whois.services;
 
+import net.ripe.db.whois.api.rest.client.RestClient;
+import net.ripe.db.whois.api.rest.client.RestClientException;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
@@ -8,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 public class BaAppsServiceTest {
@@ -25,11 +28,34 @@ public class BaAppsServiceTest {
         mockServer = MockRestServiceServer.createServer(restTemplate);
     }
 
+    private final static String crowdToken = "snfkvjnfkjvnsdnc";
+
     @Test
-    public void shouldReturnLirs() {
-        mockServer.expect(requestTo(MOCK_BA_APPS_URL + "/api/ba-apps/lirs"))
-            .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
+    public void shouldReturnLirsForMembers() {
+        String json = checkJsonForSpecificReqType("member");
+        assertEquals(json, baAppsService.getLirs(crowdToken));
+    }
 
+    @Test
+    public void shouldReturnLirsForRipeDbOrgs() {
+        String json = checkJsonForSpecificReqType("ripe-db-orgs");
+        assertEquals(json, baAppsService.getOrganisations(crowdToken));
+    }
 
+    private String checkJsonForSpecificReqType(String type) {
+        String json = "{ \"id\": \"ORG-BLA\"}";
+
+        mockServer.expect(requestTo(MOCK_BA_APPS_URL + "/authorisation-service/v2/notification/account/" + crowdToken + "/" + type))
+            .andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+
+        return json;
+    }
+
+    @Test(expected = RestClientException.class)
+    public void shouldCrashToReturnLirsForMembers() {
+        mockServer.expect(requestTo(MOCK_BA_APPS_URL + "/authorisation-service/v2/notification/account/" + crowdToken + "/member"))
+            .andRespond(withServerError());
+
+        baAppsService.getLirs(crowdToken);
     }
 }
