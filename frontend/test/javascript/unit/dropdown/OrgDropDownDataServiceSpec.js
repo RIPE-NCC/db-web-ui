6b89ce43eccaf762ceacc39d447cde64ced41251
Mikhail Puzanov <mpuzanov@ripe.net>
2017-02-24 14:42:02 +0100

    Add meaningful test.

--- frontend/test/javascript/unit/dropdown/OrgDropDownDataServiceSpec.js
+++ frontend/test/javascript/unit/dropdown/OrgDropDownDataServiceSpec.js
@@ -1,41 +1,5 @@
 'use strict';
 
-/*
- import IHttpPromise = angular.IHttpPromise;
-
- const CONTEXT_PATH: string = "/db-web-ui";
-
- class OrgDropDownDataServiceImpl implements OrgDropDownDataService {
-
- static $inject = ['$log', '$http', '$q'];
-
- loadOrgs(callback: (x: Organisation[]) => void): void {
- let ls: IHttpPromise<{data: any}> = this.$http.get(CONTEXT_PATH + "/api/ba-apps/lirs");
- let os: IHttpPromise<{data: any}> = this.$http.get(CONTEXT_PATH + "/api/ba-apps/organisations");
-
- let combined: IHttpPromise<{data: any}[]> = this.$q.all([ls, os]);
- combined.then((o) => {
- let lirs: Lir[] = o[0].data.response.results;
- let lirOrgs: Organisation[] = lirs.map((lir) => {
- return {id: lir.orgId, name: lir.organisationName + " " + lir.regId} as Organisation;
- });
- let orgs: Organisation[] = lirOrgs.concat(o[1].data);
- callback(orgs);
- }).catch((e) => {
- console.error("Couldn't load organisations", e);
- });
- }
-
- constructor(private $log: angular.ILogService,
- private $http: ng.IHttpService,
- private $q: ng.IQService) {
- }
- }
-
- angular.module("dbWebApp").service("OrgDropDownDataService", OrgDropDownDataServiceImpl);
-
- */
-
 describe('OrgDropDownDataService', function () {
 
     beforeEach(module('dbWebApp'));
@@ -46,44 +10,63 @@ describe('OrgDropDownDataService', function () {
     beforeEach(inject(function (_OrgDropDownDataService_, _$httpBackend_) {
         $httpBackend = _$httpBackend_;
         service = _OrgDropDownDataService_;
+
+        $httpBackend.whenGET(/.*.html/).respond(200);
+        $httpBackend.flush();
     }));
 
-    it('Populate IPv4 Resources', function () {
-        $httpBackend.expectGET('/db-web-ui/api/ba-apps/lirs').respond(function () {
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('Populate organisations', function () {
+        $httpBackend.whenGET('/db-web-ui/api/ba-apps/organisations').respond(function () {
+            return [200, {
+                id: 'ORG-BLA1-RIPE',
+                name: 'Some provider'
+            }, {}];
+        });
+        $httpBackend.whenGET('/db-web-ui/api/ba-apps/lirs').respond(function () {
             return [200, {
                 response: {
                     status: 200,
-                    message: "OK",
-                    pageNumber: 1,
-                    pageSize: 1,
-                    pageCount: 1,
-                    startIndex: 1,
-                    totalSize: 1,
-                    links: [],
-                    results: [
-                        {
-                            membershipId: 3629,
-                            regId: "nl.surfnet",
-                            organisationName: "SURFnet bv",
-                            serviceLevel: "NORMAL",
-                            orgId: "ORG-Sb3-RIPE",
-                            billingPhase: 0
-                        },
-                        {
-                            membershipId: 7347,
-                            regId: "zz.example",
-                            organisationName: "Internet Provider BV",
-                            serviceLevel: "NORMAL",
-                            orgId: "ORG-EIP1-RIPE",
-                            billingPhase: 0
-                        }
-                    ]
+                    results: [{
+                        membershipId: 3629,
+                        regId: "nl.surfnet",
+                        organisationName: "SURFnet bv",
+                        serviceLevel: "NORMAL",
+                        orgId: "ORG-Sb3-RIPE",
+                        billingPhase: 0
+                    }, {
+                        membershipId: 7347,
+                        regId: "zz.example",
+                        organisationName: "Internet Provider BV",
+                        serviceLevel: "NORMAL",
+                        orgId: "ORG-EIP1-RIPE",
+                        billingPhase: 0
+                    }]
                 }
             }, {}];
         });
 
-        service.loadOrgs(function (response) {
-            // TODO: test response
+        service.getOrgs().then(function (data) {
+           expect(data).toEqual([
+               {
+                   id: 'ORG-Sb3-RIPE',
+                   name: 'SURFnet bv nl.surfnet'
+               },
+               {
+                   id: 'ORG-EIP1-RIPE',
+                   name: 'Internet Provider BV zz.example'
+               },
+               {
+                   id: 'ORG-BLA1-RIPE',
+                   name: 'Some provider'
+               }
+           ]);
         });
+
+        $httpBackend.flush();
     });
 });
