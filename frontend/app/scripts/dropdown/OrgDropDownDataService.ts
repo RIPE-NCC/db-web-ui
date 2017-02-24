726ffa9cedd0fd99456511f5d22fd6a1b5b8c3ca
Mikhail Puzanov <mpuzanov@ripe.net>
2017-02-24 15:18:33 +0100

    Extend tests, fix some corner cases.

--- frontend/app/scripts/dropdown/OrgDropDownDataService.ts
+++ frontend/app/scripts/dropdown/OrgDropDownDataService.ts
@@ -8,17 +8,31 @@ class OrgDropDownDataServiceImpl implements OrgDropDownDataService {
     static $inject = ['$log', '$http', '$q'];
 
     getOrgs(): IPromise<Organisation[]> {
+        // request LIR and organisation data in parallel
         const ls: IHttpPromise<{data: any}> = this.$http.get(CONTEXT_PATH + "/api/ba-apps/lirs");
         const os: IHttpPromise<{data: any}> = this.$http.get(CONTEXT_PATH + "/api/ba-apps/organisations");
 
         const combined: IHttpPromise<{data: any}[]> = this.$q.all([ls, os]);
 
         return combined.then((o) => {
-            let lirs: Lir[] = o[0].data.response.results;
-            let lirOrgs: Organisation[] = lirs.map((lir) => {
+            let lirs: Lir[] = [];
+            try {
+                lirs = o[0].data.response.results
+            } catch (e) {  // just swallow it
+            }
+
+            // LIR response has different structure and contains more data
+            // than we need, so we map it here
+            const lirOrgs: Organisation[] = lirs.map((lir) => {
                 return {id: lir.orgId, name: lir.organisationName + " " + lir.regId} as Organisation;
             });
-            return lirOrgs.concat(o[1].data);
+            let dbOrgs = [];
+            try {
+                dbOrgs = o[1].data;
+            } catch (e) {
+            }
+
+            return lirOrgs.concat(dbOrgs);
         });
     }
 
--- frontend/test/javascript/unit/dropdown/OrgDropDownDataServiceSpec.js
+++ frontend/test/javascript/unit/dropdown/OrgDropDownDataServiceSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('OrgDropDownDataService', function () {
+fdescribe('OrgDropDownDataService', function () {
 
     beforeEach(module('dbWebApp'));
 
@@ -15,17 +15,54 @@ describe('OrgDropDownDataService', function () {
         $httpBackend.flush();
     }));
 
-    afterEach(function() {
+    afterEach(function () {
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
     });
 
-    it('Populate organisations', function () {
+    it('Populate organisations from only Whois', function () {
         $httpBackend.whenGET('/db-web-ui/api/ba-apps/organisations').respond(function () {
+            return [200, [{id: 'ORG-Sb3-RIPE', name: 'SURFnet bv nl.surfnet'}], {}];
+        });
+        $httpBackend.whenGET('/db-web-ui/api/ba-apps/lirs').respond(function () {
+            return [200, {}, {}];
+        });
+
+        service.getOrgs().then(function (data) {
+            expect(data).toEqual([{id: 'ORG-Sb3-RIPE', name: 'SURFnet bv nl.surfnet'}]);
+        });
+
+        $httpBackend.flush();
+    });
+
+    it('Populate organisations from only LIRs', function () {
+        $httpBackend.whenGET('/db-web-ui/api/ba-apps/organisations').respond(function () {
+            return [200, [], {}];
+        });
+        $httpBackend.whenGET('/db-web-ui/api/ba-apps/lirs').respond(function () {
             return [200, {
-                id: 'ORG-BLA1-RIPE',
-                name: 'Some provider'
+                response: {
+                    status: 200,
+                    results: [{
+                        membershipId: 3629,
+                        regId: "nl.surfnet",
+                        organisationName: "SURFnet bv",
+                        orgId: "ORG-Sb3-RIPE"
+                    }]
+                }
             }, {}];
+        });
+
+        service.getOrgs().then(function (data) {
+            expect(data).toEqual([{id: 'ORG-Sb3-RIPE', name: 'SURFnet bv nl.surfnet'}]);
+        });
+
+        $httpBackend.flush();
+    });
+
+    it('Populate organisations from both sources', function () {
+        $httpBackend.whenGET('/db-web-ui/api/ba-apps/organisations').respond(function () {
+            return [200, {id: 'ORG-BLA1-RIPE', name: 'Some provider'}, {}];
         });
         $httpBackend.whenGET('/db-web-ui/api/ba-apps/lirs').respond(function () {
             return [200, {
@@ -51,20 +88,11 @@ describe('OrgDropDownDataService', function () {
         });
 
         service.getOrgs().then(function (data) {
-           expect(data).toEqual([
-               {
-                   id: 'ORG-Sb3-RIPE',
-                   name: 'SURFnet bv nl.surfnet'
-               },
-               {
-                   id: 'ORG-EIP1-RIPE',
-                   name: 'Internet Provider BV zz.example'
-               },
-               {
-                   id: 'ORG-BLA1-RIPE',
-                   name: 'Some provider'
-               }
-           ]);
+            expect(data).toEqual([
+                {id: 'ORG-Sb3-RIPE', name: 'SURFnet bv nl.surfnet'},
+                {id: 'ORG-EIP1-RIPE', name: 'Internet Provider BV zz.example'},
+                {id: 'ORG-BLA1-RIPE', name: 'Some provider'}
+            ]);
         });
 
         $httpBackend.flush();
