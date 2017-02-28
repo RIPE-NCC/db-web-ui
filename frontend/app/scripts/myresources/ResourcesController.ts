5d07186934bf8f590c9610914abe7cb53eb8f8af
Paul Etchells <etch@dhcp-21-176.ripe.net>
2017-02-28 12:20:36 +0100

    fix for scope

--- frontend/app/scripts/myresources/ResourcesController.ts
+++ frontend/app/scripts/myresources/ResourcesController.ts
@@ -1,13 +1,13 @@
 class ResourcesController {
-    public static $inject = ["$log", "MyResourcesDataService"];
+    public static $inject = ["$log", "$scope", "MyResourcesDataService"];
     public ipv4Resources: IPv4ResourceDetails[];
 
     constructor(private $log: angular.ILogService,
-                private $rootScope: angular.IRootScopeService,
+                private $scope: angular.IScope,
                 private resourcesDataService: MyResourcesDataService) {
 
         this.ipv4Resources = [];
-        $rootScope.$on("organisation-changed-event", (event: IAngularEvent, selectedOrg: Organisation) => {
+        $scope.$on("organisation-changed-event", (event: IAngularEvent, selectedOrg: Organisation) => {
             this.resourcesDataService.getIpv4Resources(selectedOrg.orgId, (response) => {
                 this.ipv4Resources = response.details;
             });
