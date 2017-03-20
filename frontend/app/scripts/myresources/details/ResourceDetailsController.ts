1d7b3d3d18cfe7cc5c594309bc3cdf4b3d925974
Mikhail Puzanov <mpuzanov@ripe.net>
2017-03-20 16:10:00 +0100

    WIP: Add ng-table usage.

--- frontend/app/scripts/myresources/details/ResourceDetailsController.ts
+++ frontend/app/scripts/myresources/details/ResourceDetailsController.ts
@@ -1,3 +1,6 @@
+import * as angular from 'angular';
+import {NgTableParams} from "ng-table/src/core/ngTableParams";
+
 interface IResourceItemControllerState extends ng.ui.IStateService {
     params: {
         objectName: string;
@@ -11,19 +14,24 @@ class ResourceDetailsController {
     public whoisResponse: IWhoisResponseModel;
     public results: IWhoisObjectModel[];
     public details: IWhoisObjectModel;
-    public moreSpecifics: IMoreSpecificResource[];
+    public moreSpecificsData: IMoreSpecificResource[];
+    public moreSpecifics: NgTableParams<IMoreSpecificResource>;
 
     constructor(private $log: angular.ILogService,
                 private $state: IResourceItemControllerState,
                 private queryParametersService: IQueryParametersService,
                 private moreSpecificsService: IMoreSpecificsService) {
 
+        const self = this;
         moreSpecificsService.getSpecifics($state.params['objectName']).then(
             (response: IHttpPromiseCallbackArg<IMoreSpecificsApiResult>) => {
-                this.moreSpecifics = response.data.resources;
+                self.moreSpecificsData = response.data.resources;
+                self.moreSpecifics = new NgTableParams<IMoreSpecificResource>({}, {
+                    dataset: self.moreSpecificsData,
+                    total: self.moreSpecificsData.length
+                });
 
-
-                $log.info("more specifics: ", this.moreSpecifics);
+                $log.info("more specifics: ", this.moreSpecificsData);
             }
         );
 
