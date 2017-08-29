f5e866faa117e674c27179dd3d3ff31ee91abcde
Paul Etchells <petchells@ripe.net>
2017-08-29 16:44:29 +0200

    take control of popover timeouts

--- frontend/app/scripts/myresources/FlagComponent.ts
+++ frontend/app/scripts/myresources/FlagComponent.ts
@@ -1,9 +1,28 @@
 class FlagController {
 
+    public static $inject = ["$timeout"];
     public text: string;
     public tooltip: string;
+    public isOpen = false;
+    public timer: any;
 
-    constructor() {
+    constructor(private $timeout: any) {
+    }
+
+    public in() {
+        this.isOpen = true;
+        if (this.timer) {
+            this.$timeout.cancel(this.timer);
+        }
+    }
+
+    public out() {
+        if (this.isOpen) {
+            this.timer = this.$timeout(() => {
+                this.isOpen = false;
+                this.timer = null;
+            }, 250);
+        }
     }
 }
 
