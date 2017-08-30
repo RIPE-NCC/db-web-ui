68a6d8da65f43540ffdee66aa4fac22c589275bd
Paul Etchells <petchells@ripe.net>
2017-08-30 14:51:30 +0200

    link colour changed as per Rehan's request

--- frontend/app/scripts/LabelService.ts
+++ frontend/app/scripts/LabelService.ts
@@ -4,7 +4,7 @@ interface ILabelService {
 }
 
 const LABELS = {
-    "noContractText": "This resource is not covered by an agreement with the RIPE NCC. <a href='https://www.ripe.net/manage-ips-and-asns/legacy-resources/ripe-ncc-services-to-legacy-internet-resource-holders#i-am-a-legacy-internet-resource-holder-and-i-want-to-' target='_blank'>Find out more</a>",
+    "noContractText": "This resource is not covered by an agreement with the RIPE NCC.<br> <a href='https://www.ripe.net/manage-ips-and-asns/legacy-resources/ripe-ncc-services-to-legacy-internet-resource-holders#i-am-a-legacy-internet-resource-holder-and-i-want-to-' target='_blank'>Find out more</a>",
     "otherSponsorText": "Sponsored by another LIR"
 };
 
