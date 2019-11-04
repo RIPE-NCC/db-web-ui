e9b8c3fef5d473632fd715114a4f8f86a931be89
Ivana Svonja <isvonja@ripe.net>
2019-11-04 18:46:16 +0100

    fixed menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component} from "@angular/core";
 import {PropertiesService} from "./properties.service";
 
+declare var init_mega_menu: any;
+declare var init_popover: any;
+declare var whois_search: any;
+
 @Component({
   selector: "app-db-web-ui",
   templateUrl: "app.component.html",
@@ -8,5 +12,11 @@ import {PropertiesService} from "./properties.service";
 export class AppComponent {
 
   constructor(public properties: PropertiesService) {
+  }
+
+  public ngOnInit() {
+    init_mega_menu();
+    init_popover();
+    whois_search();
   }
 }
