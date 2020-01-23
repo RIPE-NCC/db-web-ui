848fbfca6cea2d45b46e1c42acce40996b46b19b
Ivana Svonja <isvonja@ripe.net>
2020-01-23 13:59:10 +0100

    DB-2692 fix karma test

--- frontend/test/unit/version/whois-version.component.spec.ts
+++ frontend/test/unit/version/whois-version.component.spec.ts
@@ -1,25 +1,23 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 import {WhoisVersionComponent} from "../../../src/app/version/whois-version.component";
-import {IVersion} from "../../../src/app/shared/whois-response-type.model";
 
 describe("WhoisVersionComponent", () => {
-  let component: WhoisVersionComponent;
-  let fixture: ComponentFixture<WhoisVersionComponent>;
+    let component: WhoisVersionComponent;
+    let fixture: ComponentFixture<WhoisVersionComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WhoisVersionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [WhoisVersionComponent]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WhoisVersionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WhoisVersionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
