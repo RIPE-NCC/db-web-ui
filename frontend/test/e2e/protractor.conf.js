233673ccad4e0e27aa2459aac49d66b929498e81
Sander Buskens <sbuskens@ripe.net>
2020-02-11 18:18:48 +0100

    Settings to get Chrome80 running with protractor/webdriver-manager's chrome 80 driver..

--- frontend/test/e2e/protractor.conf.js
+++ frontend/test/e2e/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
   ],
   capabilities: {
     'browserName': 'chrome',
-    'chromeOptions': {'args': ['--disable-extensions', '--window-size=1920,1080']}
+    'chromeOptions': {'args': ['--disable-extensions', '--window-size=1920,1080', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']}
   },
   directConnect: true,
   framework: 'jasmine',
