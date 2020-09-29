40e763e4f65105e48eb65f5fafdec07b077fd718
Ed Shryane <eshryane@ripe.net>
2020-09-29 13:29:06 +0200

    Debug issue with Angular path(s)

--- backend/src/main/java/net/ripe/whois/AngularResourcesFilter.java
+++ backend/src/main/java/net/ripe/whois/AngularResourcesFilter.java
@@ -1,10 +1,11 @@
 package net.ripe.whois;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,13 +17,18 @@ import java.io.IOException;
 @Component
 public class AngularResourcesFilter implements Filter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AngularResourcesFilter.class);
+
     @Override
     public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws IOException, ServletException {
 
         final HttpServletResponse response = (HttpServletResponse) res;
         final HttpServletRequest request = (HttpServletRequest) req;
 
-        String uri = request.getRequestURI().substring(((HttpServletRequest) req).getContextPath().length());
+        String uri = request.getRequestURI().substring(request.getContextPath().length());
+
+        LOGGER.info("requestURI={} uri={} Angular path? {}", request.getRequestURI(), uri, isAngularPath(uri));
+
         if (isAngularPath(uri)) {
             chain.doFilter(new HttpServletRequestWrapper(request) {
                 @Override
@@ -48,13 +54,4 @@ public class AngularResourcesFilter implements Filter {
                 uri.startsWith("/error") ||
                 uri.startsWith("/not-found");
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
-
-    @Override
-    public void destroy() {
-    }
-
 }
