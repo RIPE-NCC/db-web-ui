f17bdf955a88a0de1d242146af6ac1b7dec76a11
Ed Shryane <eshryane@ripe.net>
2020-09-30 09:40:05 +0200

    Added logging

--- backend/src/main/java/net/ripe/whois/AngularResourcesFilter.java
+++ backend/src/main/java/net/ripe/whois/AngularResourcesFilter.java
@@ -3,6 +3,7 @@ package net.ripe.whois;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -19,23 +20,67 @@ public class AngularResourcesFilter implements Filter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AngularResourcesFilter.class);
 
+    private static final String REDIRECT_PATH = "/index.html";
+
     @Override
     public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws IOException, ServletException {
 
         final HttpServletResponse response = (HttpServletResponse) res;
         final HttpServletRequest request = (HttpServletRequest) req;
 
-        String uri = request.getRequestURI().substring(request.getContextPath().length());
+        final String uri = request.getRequestURI().substring(request.getContextPath().length());
 
-        LOGGER.info("requestURI={} uri={} Angular path? {}", request.getRequestURI(), uri, isAngularPath(uri));
+        // requestURI=/db-web-ui/query uri=/query Angular path? true ****************************
+        LOGGER.info(
+            "\n\turi={}" +
+            "\n\tisAngularPath? {}" +
+            "\n\trequestURI={}" +
+            "\n\tservlet path={}" +
+            "\n\tpath info={}" +
+            "\n\tpath translated={}" +
+            "\n\tcontext path={}" +
+            "\n\trequestURL={}" +
+            "\n\tservlet path={}",
+            uri,
+            isAngularPath(uri),
+            request.getRequestURI(),
+            request.getServletPath(),
+            request.getPathInfo(),
+            request.getPathTranslated(),
+            request.getContextPath(),
+            request.getRequestURL(),
+            request.getServletPath());
 
         if (isAngularPath(uri)) {
-            chain.doFilter(new HttpServletRequestWrapper(request) {
-                @Override
-                public String getServletPath() {
-                    return "/index.html";
-                }
-            }, response);
+            chain.doFilter(
+                new HttpServletRequestWrapper(request) {
+                    @Override
+                    public String getServletPath() {
+                        return REDIRECT_PATH;
+                    }
+
+                    @Override
+                    public String getRequestURI() {
+                        return REDIRECT_PATH;
+                    }
+
+                    @Override
+                    public StringBuffer getRequestURL() {
+                        return new StringBuffer(REDIRECT_PATH);
+                    }
+
+                    @Override
+                    public Object getAttribute(final String name) {
+                        if (WebUtils.INCLUDE_SERVLET_PATH_ATTRIBUTE.equals(name)) {
+                            LOGGER.info("getAttribute: {}={}", name, REDIRECT_PATH);
+                            return REDIRECT_PATH;
+                        }
+
+                        LOGGER.info("getAttribute: {}={}", name, super.getAttribute(name));
+                        return super.getAttribute(name);
+                    }
+                },
+                response);
         } else {
             chain.doFilter(req, res);
         }
