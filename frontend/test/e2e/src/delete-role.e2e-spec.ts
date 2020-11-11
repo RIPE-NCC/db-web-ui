3ba212da316aa2659034fe05c4c0f886cc59980d
Ivana Svonja <isvonja@ripe.net>
2020-11-11 22:47:09 +0100

    DB-2871 Bugfix, reason of deleting object pick up from field

---
+++ frontend/test/e2e/src/delete-role.e2e-spec.ts
@@ -0,0 +1,24 @@
+import {browser} from "protractor";
+
+const page = require("./homePageObject");
+const fs = require("fs");
+
+describe("Delete a role object", () => {
+
+    it("should be able to pick up reason of deleting object", () => {
+        browser.get(browser.baseUrl + "webupdates/modify/ripe/role/IA6414-RIPE");
+        expect(page.modal.isPresent()).toBe(false);
+        page.scrollIntoView(page.btnDeleteObject);
+        page.btnDeleteObject.click();
+        expect(page.modal.isPresent()).toBe(true);
+        page.modalInpReason.clear();
+        page.modalInpReason.sendKeys("my own reason");
+        page.scrollIntoView(page.btnConfirmDeleteObject);
+        expect(page.btnConfirmDeleteObject.isPresent()).toBeTruthy();
+        page.btnConfirmDeleteObject.click();
+        // won't get to display page in case reason is not "my own reason", because we have mock of response for
+        // this reason. Otherwise it will generate new *.json.404
+        expect(browser.getCurrentUrl()).toContain("webupdates/delete/ripe/role/IA6414-RIPE?onCancel=webupdates%2Fmodify");
+    });
+
+});
