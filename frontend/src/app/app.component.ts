e511443942cd4f7bd032a2d43c8cc8e4d755e29b
Ivana Svonja <isvonja@ripe.net>
2021-02-26 15:30:36 +0100

    Close menu for loaded app in mobile or on resize breaking point 768

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from "@angular/core";
+import {Component, HostListener, Inject} from "@angular/core";
 import {PropertiesService} from "./properties.service";
 import {WINDOW} from "./core/window.service";
 import {Router} from "@angular/router";
@@ -11,7 +11,8 @@ export class AppComponent {
 
   public isIE: boolean;
   public isIEOrEdge: boolean;
-  public isOpenMenu: boolean = true;
+  public isOpenMenu: boolean;
+  private innerWidth: number;
 
   constructor(public properties: PropertiesService,
               private router: Router,
@@ -23,6 +24,7 @@ export class AppComponent {
     this.isIE = /msie\s|trident\//i.test(window.navigator.userAgent);
     // TODO isIEOrEdge REMOVE AFTER 1st March 2021
     this.isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent);
+    this.openOrCloseMenu();
   }
 
   private skipHash() {
@@ -32,7 +34,17 @@ export class AppComponent {
     }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.openOrCloseMenu();
+  }
+
   open = (event: any) => {
     this.isOpenMenu = event.detail.open;
+  }
+
+  public openOrCloseMenu() {
+    this.innerWidth = window.innerWidth;
+    this.isOpenMenu = this.innerWidth >= 768;
   }
 }
