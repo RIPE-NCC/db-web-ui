69229d849c2565e3709d52f1cd84ff7e0f3d299c
Mahesh Aggarwal <maggarwal@ripe.net>
2021-12-28 17:12:36 +0000

    Use zendesk chat phase 4

--- frontend/src/app/livechat/live-chat.component.ts
+++ frontend/src/app/livechat/live-chat.component.ts
@@ -25,18 +25,6 @@ export class LiveChatComponent {
   }
 
   public isHideLiveChat() {
-    if (this.properties.LIVE_CHAT_KEY === "") {
-      return true;
-    }
-
-    const date = new Date();
-    const currentDay = date.toLocaleDateString("en-GB", {timeZone: "Europe/Amsterdam", weekday: "long"})
-    // don't show for weekends
-    const isWeekend = currentDay === "Saturday" || currentDay === "Sunday"
-
-    const hoursMinutesSeconds = date.toLocaleTimeString("en-GB", {timeZone: "Europe/Amsterdam"}).split(":").map(x => parseInt(x))
-    // don't show before 9h and after 18h
-    const isWorkingHours = hoursMinutesSeconds[0] >= 9 && hoursMinutesSeconds[0] < 18;
-    return isWeekend || !isWorkingHours;
+    return this.properties.LIVE_CHAT_KEY === "";
   }
 }
