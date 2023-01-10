86fd601f0c697a0b7f3c0629c880cc10eee2bf3d
Mahesh Aggarwal <maggarwal@ripe.net>
2023-01-10 09:08:29 +0000

    Java 17

--- frontend/test/cypress/integration/session-expire.e2e-spec.ts
+++ frontend/test/cypress/integration/session-expire.e2e-spec.ts
@@ -6,36 +6,46 @@ describe('Session expire', () => {
     const userInfoFile = './test/e2e/mocks/e2eTest/35076578e970f4e6bca92a8f746671291eec84b0.json';
     const userWithAllRoles = './test/e2e/mocks/e2eTest/user-with-all-role.json';
 
-    after(() => {
+    afterEach(() => {
         cy.changeJsonResponseFile(userWithAllRoles, userInfoFile);
     });
 
     it('should show the banner when the user is logged and the session is expired', () => {
         // we can't use prism to intercept calls to other domains
-        cy.intercept('GET', 'https://access.prepdev.ripe.net/user/profile', { statusCode: 200 }).as('getProfile');
+        cy.intercept('GET', 'https://access.prepdev.ripe.net/user/profile', { statusCode: 200, body: {} }).as('getProfile');
+        cy.intercept('GET', 'https://access.ripe.net/user/profile', { statusCode: 200, body: {} });
         cy.setCookie('crowd.ripe.hint', 'true');
         queryPage.visit();
         cy.wait('@getProfile');
         cy.changeJsonResponseFile(userNotLoggedIn, userInfoFile);
+        cy.wait('@getProfile');
+        cy.intercept('GET', /https:\/\/localhost(.ripe.net)?:9002\/db-web-ui\/api\/whois-internal\/api\/user\/info/).as('getUserInfo');
+        cy.wait('@getUserInfo');
         queryPage.expectUserLoggedImage(false).expectErrorMessageToContain('Your RIPE NCC Access session has expired. You need to login again.');
     });
 
     it('should show logged out icon if the user is not logged', () => {
         // we can't use prism to intercept calls to other domains
         cy.intercept('GET', 'https://access.prepdev.ripe.net/user/profile', { statusCode: 401 }).as('getProfile');
+        cy.intercept('GET', 'https://access.ripe.net/user/profile', { statusCode: 401 });
         cy.setCookie('crowd.ripe.hint', 'true');
         queryPage.visit();
         cy.wait('@getProfile');
+        cy.intercept('GET', /https:\/\/localhost(.ripe.net)?:9002\/db-web-ui\/api\/whois-internal\/api\/user\/info/).as('getUserInfo');
+        cy.wait('@getUserInfo');
         queryPage.expectUserLoggedImage(false);
     });
 
     it('should show the logged icon if the user is logged', () => {
         // we can't use prism to intercept calls to other domains
-        cy.intercept('GET', 'https://access.prepdev.ripe.net/user/profile', { statusCode: 200 }).as('getProfile');
+        cy.intercept('GET', 'https://access.prepdev.ripe.net/user/profile', { statusCode: 200, body: {} }).as('getProfile');
+        cy.intercept('GET', 'https://access.ripe.net/user/profile', { statusCode: 200, body: {} });
         cy.changeJsonResponseFile(userWithAllRoles, userInfoFile);
         cy.setCookie('crowd.ripe.hint', 'true');
         queryPage.visit();
         cy.wait('@getProfile');
+        cy.intercept('GET', /https:\/\/localhost(.ripe.net)?:9002\/db-web-ui\/api\/whois-internal\/api\/user\/info/).as('getUserInfo');
+        cy.wait('@getUserInfo');
         queryPage.expectUserLoggedImage(true);
     });
 });
