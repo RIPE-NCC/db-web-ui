459d4b5e46cd53aed597522e269c6f666ed99ddb
Mahesh Aggarwal <maggarwal@ripe.net>
2024-03-08 12:17:22 +0100

    run_prettier

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -38,11 +38,11 @@ import { SyncupdatesComponent } from './syncupdates/syncupdates.component';
 import { SyncupdatesService } from './syncupdates/syncupdates.service';
 import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 import { UnsubscribeService } from './unsubscribe/unsubscribe.service';
+import { UnsubscribeConfirmationComponent } from './unsubscribeConfirmation/unsubscribe-confirm.component';
 import { UpdatesTextModule } from './updatestext/update-text.module';
 import { UpdatesWebModule } from './updatesweb/updateweb.module';
 import { UserInfoModule } from './userinfo/user-info.module';
 import { WhoisObjectModule } from './whois-object/whois-object.module';
-import {UnsubscribeConfirmationComponent} from "./unsubscribeConfirmation/unsubscribe-confirm.component";
 
 @NgModule({
     declarations: [
