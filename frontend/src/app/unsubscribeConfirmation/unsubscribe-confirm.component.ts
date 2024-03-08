466b52037875b1ac24759868ec945a603142e052
Mahesh Aggarwal <maggarwal@ripe.net>
2024-03-08 11:07:31 +0100

    Get email from whois-internal via message id

--- frontend/src/app/unsubscribeConfirmation/unsubscribe-confirm.component.ts
+++ frontend/src/app/unsubscribeConfirmation/unsubscribe-confirm.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UnsubscribeConfirmService } from './unsubscribe-confirm.service';
 
 @Component({
     templateUrl: './unsubscribe-confirm.component.html',
 })
 export class UnsubscribeConfirmationComponent implements OnInit {
     public messageId: string;
+    public email: string;
+    public loading: boolean = true;
+    public isSucess: boolean = false;
 
-    constructor(public activatedRoute: ActivatedRoute) {}
+    constructor(public unsubscribeConfirmService: UnsubscribeConfirmService, public activatedRoute: ActivatedRoute) {}
 
     public ngOnInit() {
         this.messageId = this.activatedRoute.snapshot.paramMap.get('messageId');
+        this.unsubscribeConfirmService.getEmailFromMessageId(this.messageId).subscribe({
+            next: (response: string) => {
+                this.email = response;
+                this.loading = false;
+                this.isSucess = true;
+            },
+            error: () => {
+                this.loading = false;
+                this.isSucess = false;
+            },
+        });
     }
 
     public confirm() {
---
+++ frontend/src/app/unsubscribeConfirmation/unsubscribe-confirm.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SKIP_HEADER } from '../interceptor/header.interceptor';
+
+@Injectable()
+export class UnsubscribeConfirmService {
+    private readonly URL: string = `api/whois-internal/public/getEmailFromMessageId`;
+
+    constructor(private http: HttpClient) {}
+
+    public getEmailFromMessageId(messageId: string): Observable<any> {
+        if (!messageId) {
+            console.error('Unsubscribing email', messageId);
+            throw new TypeError('UnsubscribeConfirmService.unsubscribe failed: no messageId');
+        }
+        const headers = new HttpHeaders().set('content-type', 'text/plain').set(SKIP_HEADER, '');
+        return this.http.get(this.URL, messageId, { headers, responseType: 'text' as 'json' });
+    }
+}
