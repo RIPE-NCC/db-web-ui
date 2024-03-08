38f3e6dc2ac22bc1c8b5f3c58e279998fa9774ee
Mahesh Aggarwal <maggarwal@ripe.net>
2024-03-08 16:17:12 +0100

    add_loggings

--- frontend/src/app/unsubscribeConfirmation/unsubscribe-confirm.component.ts
+++ frontend/src/app/unsubscribeConfirmation/unsubscribe-confirm.component.ts
@@ -17,7 +17,7 @@ export class UnsubscribeConfirmationComponent implements OnInit {
         this.messageId = this.activatedRoute.snapshot.paramMap.get('messageId');
 
         this.unsubscribeConfirmService.getEmailFromMessageId(this.messageId).subscribe({
-            next: (response: any) => {
+            next: (response: string) => {
                 console.log('email is : ' + response);
 
                 this.email = response;
