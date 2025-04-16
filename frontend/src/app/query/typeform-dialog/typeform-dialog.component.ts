ed7357d00112695469e256c79d3f6fcd2517d903
Ivana Svonja <isvonja@ripe.net>
2025-04-16 07:48:22 +0000

    [DB-6184] embedded typeform

---
+++ frontend/src/app/query/typeform-dialog/typeform-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { createPopup } from '@typeform/embed';
+
+@Component({
+    selector: 'typeform-dialog',
+    templateUrl: 'typeform-dialog.component.html',
+    styleUrl: 'typeform-dialog.component.scss',
+})
+export class TypeformDialogComponent implements OnInit {
+    constructor(private dialogRef: MatDialogRef<TypeformDialogComponent>) {}
+
+    ngOnInit() {
+        createPopup('sFTT41YW', {
+            container: document.getElementById('tf-popover'),
+            open: 'load',
+            height: '100%',
+            width: '100%',
+            hideFooter: true,
+            iframeProps: {
+                width: '100%',
+            },
+            onClose: () => this.close(),
+        });
+    }
+
+    close(): void {
+        this.dialogRef.close();
+    }
+}
