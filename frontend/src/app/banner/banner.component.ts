680b8fdd99a0701ccdffc0d87b2114793f5d7427
isvonja <isvonja@ripe.net>
2025-09-17 22:04:44 +0200

    DB-6614 replaced app-banner with inner banner component build by latest design

---
+++ frontend/src/app/banner/banner.component.ts
@@ -0,0 +1,73 @@
+import { NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatDialogClose } from '@angular/material/dialog';
+
+/*  Usage
+    promo:
+        - Upcoming events (Ripe meeting / training)
+        - New features
+        - Product release
+    informational:
+        - Welcome Message
+        - Policy Changes: Notify users about updates to terms of service, privacy policies, or other legal agreements that affect their use of the platform. (not warning / dangerous)
+        - Helpful Tips: Offer users tips, tricks, or best practices for using the platform more effectively or efficiently.
+    error:
+        - Network or Server Errors: Inform users about issues related to network connectivity or server errors, such as timeouts, database connection failures, or server maintenance.
+        - Expired sessions
+        - Data Processing Errors
+        - System-wide Errors: Banner errors can be used to communicate system-wide errors or disruptions, such as service outages, technical issues, or maintenance downtime, and inform users about the situation and expected resolution times
+    warning:
+        - Potential risks or errors
+        - Service Updates: Inform users about scheduled maintenance, system upgrades that will might effect the normal workflow and they need to be aware of.
+    success:
+        - A success banner notification is used to provide positive feedback to users and reassure them that their actions or tasks have been completed successfully.
+        - Form Submission: After successfully submitting a form, such as a registration form or a contact form, a success banner can confirm that the submission was successful
+        - Account Creation: After successfully creating a new account on a website or application, a success banner can confirm the account creation and provide next steps for the user.
+        - Task Completion: In task-oriented applications, such as project management tools or to-do lists, a success banner can notify users when a task has been completed successfully.
+        - File Upload: After successfully uploading a file, such as a document or an image, a success banner can confirm the successful upload and provide options for further actions.
+     */
+export enum BannerTypes {
+    promo = 'promo',
+    informational = 'informational',
+    error = 'error',
+    warning = 'warning',
+    success = 'success',
+}
+
+@Component({
+    selector: 'banner',
+    templateUrl: './banner.component.html',
+    styleUrl: 'banner.component.scss',
+    imports: [NgIf, MatButton, MatDialogClose],
+    standalone: true,
+})
+export class BannerComponent implements OnInit {
+    @Input()
+    title?: string;
+    @Input()
+    textContent: string;
+    @Input()
+    type: BannerTypes;
+    @Input() // used for closing specific banner
+    id: string;
+    @Input() // dismiss once and for all
+    persistDismiss: boolean = false;
+
+    closed: boolean;
+
+    ngOnInit() {
+        if (this.persistDismiss) {
+            this.closed = localStorage.getItem(this.id) === 'closed';
+        }
+    }
+
+    closeBanner() {
+        this.closed = true;
+        if (this.persistDismiss) {
+            localStorage.setItem(this.id, 'closed');
+        }
+    }
+
+    protected readonly BannerTypes = BannerTypes;
+}
