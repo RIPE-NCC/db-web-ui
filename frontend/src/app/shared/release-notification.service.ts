8fbb16056fbd7c9e58948ab8fb66a6659bb0f3e0
isvonja <isvonja@ripe.net>
2025-09-30 12:38:39 +0200

    new release reload

--- frontend/src/app/shared/release-notification.service.ts
+++ frontend/src/app/shared/release-notification.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { timer } from 'rxjs';
 import { IProperties, PropertiesService } from '../properties.service';
 import { AlertsService } from './alert/alerts.service';
@@ -10,7 +11,7 @@ export class ReleaseNotificationService {
     private pollInterval: number;
     private isBannerShown: boolean = false;
 
-    constructor(propertiesService: PropertiesService, private httpClient: HttpClient, private alertsService: AlertsService) {
+    constructor(propertiesService: PropertiesService, private httpClient: HttpClient, private alertsService: AlertsService, private router: Router) {
         this.buildTime = propertiesService.DB_WEB_UI_BUILD_TIME;
         this.pollInterval = propertiesService.RELEASE_NOTIFICATION_POLLING;
     }
@@ -32,7 +33,7 @@ export class ReleaseNotificationService {
                 if (this.buildTime !== newBuildTime && !this.isBannerShown) {
                     this.buildTime = newBuildTime;
                     this.isBannerShown = true;
-                    this.alertsService.addGlobalWarning('There is a new release available. Click reload to start using it.', document.location.href, 'RELOAD');
+                    this.alertsService.addGlobalWarning('There is a new release available. Click reload to start using it.', this.router.url, 'RELOAD');
                 }
             });
         });
