a9598f0960454e2807543358d790412be4434460
mherran <mherran@ripe.net>
2025-10-24 15:05:13 +0200

    feat: fix value

--- frontend/src/app/updatesweb/create-modify.component.ts
+++ frontend/src/app/updatesweb/create-modify.component.ts
@@ -188,12 +188,12 @@ export class CreateModifyComponent implements OnInit, OnDestroy {
             this.mntnerService.getAuthForObjectIfNeeded(parentObject, this.maintainers.sso, this.operation, this.source, this.objectType, this.name).subscribe({
                 next: (result: any) => {
                     console.log('Got value:', result);
-                    console.log('Got value1:', result.value);
-                    console.log('Got value3:', result.value.selectedItem);
+                    console.log('Got value1:', result.$value);
+                    console.log('Got value2:', result.$value.selectedItem);
                     this.restCallInProgress = false;
                     this.inetnumParentAuthError = false;
-                    if (!result.value) {
-                        this.maintainers.sso.push(result.value.selectedItem);
+                    if (!result.$value) {
+                        this.maintainers.sso.push(result.$value.selectedItem);
                     }
                 },
                 error: (error: any) => {
