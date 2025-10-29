c9886ff2dc2586b2822014162a2abae5e88ef617
Miguel Herrán <106821523+MiguelAHM@users.noreply.github.com>
2025-10-29 12:36:37 +0100

    Do Not Display Auth Modal If previous Auth Succeeded (#68)
    * feat: add logs
    
    * feat: add more logs
    
    * feat: fetch mntners with SSO when parent is found
    
    * feat: add log
    
    * feat: processParent after updating SSO mntners
    
    * feat: try other way
    
    * feat: update sso mntners when loggedin
    
    * feat: add more logs
    
    * feat: fix value
    
    * feat: remove the if condition
    
    * feat: avoid error when undefined
    
    * feat: remove logs and add test case
    
    * feat: fix EE test
    
    * feat: remove not necesary code
    
    * feat: add logs
    
    * feat: remove logs
    
    * feat: add logs
    
    * feat: add mntners to both object and sso
    
    * feat: add logs
    
    * feat: refresh mntner editor component
    
    * feat: add mntners refresh
    
    * feat: fix Test
    
    * feat: update just if any mntner has been updated
    
    * feat: remove e2e test
    
    * feat: add logs
    
    * feat: update attributes
    
    * feat: use the correct attribute list
    
    * feat: use just attributes
    
    * feat: add logs
    
    * feat: use addSelectedAttribute
    
    * feat: add logs
    
    * feat: add logs
    
    * feat: little refactor
    
    * feat: try with enrich
    
    * feat: just refresh mntner editr
    
    * feat: remove not needed data
    
    * feat: use viewChild

--- frontend/src/app/whois-object/maintainers-editor.component.ts
+++ frontend/src/app/whois-object/maintainers-editor.component.ts
@@ -316,7 +316,7 @@ export class MaintainersEditorComponent implements OnInit {
         return createdAttr && typeof createdAttr.value === 'string' && createdAttr.value.length > 0;
     }
 
-    private initCreateMode() {
+    public initCreateMode() {
         this.restService.fetchMntnersForSSOAccount().subscribe({
             next: (results: IMntByModel[]) => {
                 this.handleSsoResponse(results);
