570d829a71e6846898195634e9d9f7d6c30e5f68
isvonja <isvonja@ripe.net>
2026-06-16 22:51:23 +0200

    DB-7291 removed useless code

--- frontend/src/app/authentication-guard.service.ts
+++ frontend/src/app/authentication-guard.service.ts
@@ -1,38 +1,24 @@
 import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { UserOidc } from './dropdown/org-data-type.model';
-import { PropertiesService } from './properties.service';
 import { UserInfoService } from './userinfo/user-info.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationGuard {
     private userInfoService = inject(UserInfoService);
-    private properties = inject(PropertiesService);
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    canActivate(): Observable<boolean> | Promise<boolean> | boolean {
         return this.userInfoService.getLoggedInOidc().pipe(
             map((userOidc: UserOidc) => true),
             catchError(() => {
-                this.redirectToLogin(state.url);
+                this.redirectToLogin();
                 return of(false);
             }),
         );
-        // return this.userInfoService.getUserOrgsAndRoles().pipe(
-        //     map((userInfo: IUserInfoResponseData) => true),
-        //     catchError(() => {
-        //         this.redirectToLogin(state.url);
-        //         return of(false);
-        //     }),
-        // );
     }
 
-    private redirectToLogin(originalPath: string) {
-        // const url = window.location.origin + `/db-web-ui${originalPath}`;
-        // const ssoUrl = `${this.properties.LOGIN_URL}?originalUrl=${encodeURIComponent(url)}`;
-        // console.info('Force SSO login:' + ssoUrl);
-        // window.location.href = ssoUrl;
+    private redirectToLogin() {
         window.location.href = `/db-web-ui/oauth2/authorization/keycloak?next=${window.location.href}`;
     }
 }
