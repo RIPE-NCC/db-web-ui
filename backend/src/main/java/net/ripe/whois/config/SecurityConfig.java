909603f9bba93a6c8346e2345e923cc41ca5aacc
isvonja <isvonja@ripe.net>
2026-06-24 09:33:34 +0200

    DB-7291 added "index.html" to path which doen't need authorization

--- backend/src/main/java/net/ripe/whois/config/SecurityConfig.java
+++ backend/src/main/java/net/ripe/whois/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/",
+                        "/index.html",
                         "/assets/**",
                         "/media/**", // fontawesome
                         "/*.js",
@@ -61,7 +62,7 @@ public class SecurityConfig {
                         "/legal",
                         "/error",
                         "/not-found").permitAll()
-                .requestMatchers("/public/**", "/api/healthcheck", "/api/whois-internal/api/user/info", "/api/metadata/help", "/api/whois/search").permitAll()
+                .requestMatchers("/public/**", "/api/healthcheck", "/api/whois-internal/api/user/info", "/api/metadata/help", "/api/whois/search", "/api/whois/ripe/mntner/*").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2Login(oauth -> {
