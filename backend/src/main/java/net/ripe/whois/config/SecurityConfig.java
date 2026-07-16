63944c1f84cc761f16a0696557d8f5cf2152249a
isvonja <isvonja@ripe.net>
2026-07-16 22:36:33 +0200

    added api/syncupdates to SecurityConfig

--- backend/src/main/java/net/ripe/whois/config/SecurityConfig.java
+++ backend/src/main/java/net/ripe/whois/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
                         "/legal",
                         "/error",
                         "/not-found").permitAll()
-                .requestMatchers("/public/**", "/api/healthcheck", "/api/whois-internal/api/user/info","/api/metadata/help", "/api/whois/search", "/api/whois/ripe/**").permitAll()
+                .requestMatchers("/public/**", "/api/healthcheck", "/api/syncupdates", "/api/whois-internal/api/user/info","/api/metadata/help", "/api/whois/search", "/api/whois/ripe/**").permitAll()
                 .anyRequest().authenticated()
             )
             .csrf(AbstractHttpConfigurer::disable)
