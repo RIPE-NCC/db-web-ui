19c49e7218cd3a8f3f9b5083571de9cae8d259dd
isvonja <isvonja@ripe.net>
2026-07-17 12:30:17 +0200

    revert e2e test

--- frontend/test/cypress/pages/webupdates.page.ts
+++ frontend/test/cypress/pages/webupdates.page.ts
@@ -33,7 +33,7 @@ export class WebupdatesPage {
     }
 
     typeOnField(fieldName: string, text: string) {
-        cy.get(`#createForm [name^='${fieldName}']`).clear({ force: true }).type(text, { force: true, delay: 10 });
+        cy.get(`#createForm [name^='${fieldName}']`).clear({ force: true }).type(text, { force: true });
         return this;
     }
 
