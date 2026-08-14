a6e9de243448b65ba87602e8dc7d9f64b2c219f1
isvonja <isvonja@ripe.net>
2026-08-14 11:58:45 +0200

    DB-7468 sanitizeHtml in diff directive

--- frontend/src/app/shared/whois-line-diff.directive.ts
+++ frontend/src/app/shared/whois-line-diff.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, Input, OnChanges, OnInit, inject } from '@angular/core';
 import DiffMatchPatch, { Diff } from 'diff-match-patch';
+import sanitizeHtml from 'sanitize-html';
 
 @Directive({ selector: '[whoisLineDiff]', standalone: true })
 export class WhoisLineDiffDirective implements OnInit, OnChanges {
@@ -30,19 +31,27 @@ export class WhoisLineDiffDirective implements OnInit, OnChanges {
         if (typeof this.right === 'number' || typeof this.right === 'boolean') {
             this.right = this.right.toString();
         }
-        this.el.nativeElement.innerHTML = this.createHtml(this.getLineDiff(this.left, this.right));
+
+        const html = this.createHtml(this.getLineDiff(this.left, this.right));
+
+        this.el.nativeElement.innerHTML = sanitizeHtml(html, {
+            allowedTags: ['div', 'span', 'del', 'ins'],
+            allowedAttributes: {
+                span: ['class'],
+                div: ['class'],
+            },
+        });
     }
 
-    private getLineDiff(left: string, right: string) {
+    private getLineDiff(left: string, right: string): Diff[] {
         const chars = this.dmp.diff_linesToChars_(left, right);
-        const diffs: DiffMatchPatch.Diff[] = this.dmp.diff_main(chars.chars1, chars.chars2, false);
+        const diffs = this.dmp.diff_main(chars.chars1, chars.chars2, false);
         this.dmp.diff_charsToLines_(diffs, chars.lineArray);
         return diffs;
     }
 
-    private createHtml(diffs: Array<Diff>): string {
-        let html: string;
-        html = '<div>';
+    private createHtml(diffs: Diff[]): string {
+        let html = '<div>';
         for (const diff of diffs) {
             if (diff[0] === DiffMatchPatch.DIFF_EQUAL) {
                 html += `<span class="equal">${diff[1]}</span>`;
@@ -51,14 +60,14 @@ export class WhoisLineDiffDirective implements OnInit, OnChanges {
                 const rowsDiff = diff[1].split(/\r\n|\r|\n/);
                 rowsDiff.pop();
                 for (const row of rowsDiff) {
-                    html += `<div class=\"del\"><del> - ${row} </del></div>\n`;
+                    html += `<div class="del"><del> - ${row} </del></div>\n`;
                 }
             }
             if (diff[0] === DiffMatchPatch.DIFF_INSERT) {
                 const rowsDiff = diff[1].split(/\r\n|\r|\n/);
                 rowsDiff.pop();
                 for (const row of rowsDiff) {
-                    html += `<div class=\"ins\"><ins> + ${row} </ins></div>\n`;
+                    html += `<div class="ins"><ins> + ${row} </ins></div>\n`;
                 }
             }
         }
