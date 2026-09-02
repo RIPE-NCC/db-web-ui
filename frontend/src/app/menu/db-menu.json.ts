0790fe45c77aac974cdc005e595406712b70289b
Ivana Svonja <isvonja@ripe.net>
2026-09-02 19:33:35 +0000

    [DB-7527] fulltext search not selected in menu because url changed

--- frontend/src/app/menu/db-menu.json.ts
+++ frontend/src/app/menu/db-menu.json.ts
@@ -8,13 +8,14 @@ export const dbMenuObject = {
                 url: 'query',
                 id: 'query',
                 icon: 'assets/icons/fa-database.svg',
-                active: () => location.href.includes('query') || location.href.includes('lookup'),
+                active: () => location.href.includes('/query') || location.href.includes('/lookup'),
             },
             {
                 title: 'Full Text Search',
                 url: 'fulltextsearch',
                 id: 'fulltextsearch',
                 icon: 'assets/icons/fa-magnifying-glass.svg',
+                active: () => location.href.includes('fulltextsearch'),
             },
             {
                 title: 'Syncupdates',
@@ -27,7 +28,7 @@ export const dbMenuObject = {
                 url: 'webupdates/select',
                 id: 'select',
                 icon: 'assets/icons/fa-layer-plus.svg',
-                active: () => location.href.includes('webupdates/select'),
+                active: () => location.href.includes('webupdates/select') || location.href.includes('webupdates/create'),
             },
             {
                 title: 'API Keys',
