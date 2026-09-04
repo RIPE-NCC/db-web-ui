d1958ce7aafd834275c957017fc9a9c5586932b5
mherran <mherran@ripe.net>
2026-09-04 11:36:08 +0200

    feat: set again 8 hours expiration timeout

--- backend/src/main/java/net/ripe/whois/config/OidcUtils.java
+++ backend/src/main/java/net/ripe/whois/config/OidcUtils.java
@@ -4,7 +4,7 @@ public final class OidcUtils {
 
     private OidcUtils() {}
 
-    public static final int HAZELCAST_OIDC_CACHES_TIMEOUT = 8 * 60;
+    public static final int HAZELCAST_OIDC_CACHES_TIMEOUT = 8 * 60 * 60;
 
     public static final String OIDC_LOCAL_COOKIE_NAME = "DBSESSIONID";
 
